16951d2fc9fe3847f446f7dd9540496ec05a42df
Mike Sherman <himikesherman@gmail.com>
2016-11-16 09:41:56 -0800

    Alternative solution and function valid input check added.

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,37 +1,63 @@
 // Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
-                    2,
-                    'fizz',
-                    4,
-                    'buzz',
-                    'fizz',
-                    7,
-                    8,
-                    'fizz',
-                    'buzz',
-                    11,
-                    'fizz',
-                    13,
-                    14,
-                    'fizzbuzz',
-                    16 ]
+//                     2,
+//                     'fizz',
+//                     4,
+//                     'buzz',
+//                     'fizz',
+//                     7,
+//                     8,
+//                     'fizz',
+//                     'buzz',
+//                     11,
+//                     'fizz',
+//                     13,
+//                     14,
+//                     'fizzbuzz',
+//                     16 ]
 
 function fizzbuzz(num) {
-  var arr = [];
-  for (var i = 1; i <= num; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      arr.push("fizzbuzz");
-    }
-    else if (i % 3 === 0) {
-      arr.push("fizz");
-    } else if (i % 5 === 0) {
-      arr.push("buzz");
-    } else {
-      arr.push(i);
-    }
-  }
-  
-  return arr;
+	//solution 1
+	/*
+	const arr = [];
+	let i = 1;
+	if (!isNaN(num)) {
+		for (; i <= num; i++) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				arr.push("fizzbuzz");
+			}
+			else if (i % 3 === 0) {
+				arr.push("fizz");
+			} else if (i % 5 === 0) {
+				arr.push("buzz");
+			} else {
+				arr.push(i);
+			}
+		}
+	}
+
+	return arr;
+	*/
+
+	//solution 2
+	const arr = [];
+	let el;
+	let i = 1;
+ 
+ 	if (!isNaN(num)) {
+ 		for (; i <= num; i++) {
+			el = "";
+			if (i % 3 === 0) el = "fizz";
+
+			if (i % 5 === 0) el = el + "buzz";
+
+			if (!el) el = i;
+
+			arr.push(el);
+		}
+ 	}	
+
+	return arr;
 }
 
 module.exports = fizzbuzz;
