85ecdc4cfd8b6773c03fa1ced4f101a3f9af94a8
Michael Richards <mrichards@Michaels-MacBook-Pro.local>
2016-11-18 14:44:23 -0800

    Test failed despite console looking good - trying this change.

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -20,7 +20,7 @@ function drawStairs(n){
   let totalSpaces = n;
 
   // iterate each line
-  for (let i = 1; i <= n; i++){
+  for (let i = 0; i <= n; i++){
     // create the spaces for each line
     let spaces = " ".repeat(totalSpaces);
     // add the spaces before the asterisk line
