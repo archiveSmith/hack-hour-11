033293cb12d2453de59e6da93c5c075ffbb1feec
codesmith dmrx <codesmith@Davids-MacBook-Air.local>
2016-11-18 18:17:01 -0800

    fixed to ascend/align right

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,26 +1,54 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
   ****
  *****
 ******
+
 */
 
 function drawStairs(n) {
-  let star = '';
-  while (star.length < n){
-    star += "*";
-    console.log(star);
+  if (n < 1 || n > 100) return 'unacceptable input - must be 1 to 100';
+  let star = [];
+  let result=[];
+  for (let i = n; i !== 0; i--){
+    result.push(" ");
+  }
+  while (star.length < n) {
+    star.push('*');
+    result =  result.slice(0, n - star.length).concat(star);
+    console.log(result.join(''));
   }
 }
 
 
 
+
+
+  
+
+
+
+function drawStairs(n) {
+  if (n < 1 || n > 100) return 'unacceptable input - must be 1 to 100';
+  let stars = 1;
+  let whitespace = n-1;
+  let result = '';
+ for ()
+  while (whitespace > 0) {
+    result += ' ';
+  }
+  while (star > 0){
+    result += *;
+  }
+  
+
+};
 
 module.exports = drawStairs;
