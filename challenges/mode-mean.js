66678d8dcfbe9d25b08be4e139c93bb2b32f1c49
Jimmy Huynh <jkchuynh829@gmail.com>
2016-11-19 09:58:46 -0800

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,28 @@
 
 
 function modemean(array) {
+	// LINEAR TIME COMPLEXITY;
+	// SPACE COMPLEXITY - INPUT ARRAY, ONE OBJECT
+  const tracker = {};
 
+  const mean = array.reduce((acc, curr) => {
+    const result = acc + curr;
+    return result;
+  }) / array.length;
+
+  array.forEach((num) => {
+    tracker[num] = tracker[num] ? ++tracker[num] : 1;
+  });
+
+  const mode = array.reduce((acc, curr) => {
+    const result = tracker[curr] > tracker[acc] ? curr : acc;
+    return result;
+  });
+
+  return mean === mode;
 }
+
+console.log(modemean([1, 2, 3, 3, 3, 4, 4])); // false
+console.log(modemean([10, 20, 20, 20, 30])); // true
 
 module.exports = modemean;
