3fc58e0559ce6888363bd04553c7fb40a73b15e8
Scott Schwartz <scottmschwrtz@gmail.com>
2016-11-19 10:14:29 -0800

    Finished mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,55 @@
  *
  */
 
+ function modemean(array) {
+  let total = 0;
+  let objMode = {};
+  let modeAns;
 
-function modemean(array) {
+  function mean(arr) {
+    total = arr.reduce( function(total1, el) {
+      return total += el;
+    });
 
+    return Math.floor(total/(arr.length - 1));
+  }
+
+  function mode(arr) {
+    let max = 0;
+    let ans;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (objMode[arr[i]]) {
+        objMode[arr[i]]++;
+      }
+      else {
+        objMode[arr[i]] = 1;
+      }
+    }
+
+    for (var key in objMode) {
+      if (objMode[key] === max) {
+        if (key > ans) {
+          ans = key;
+        }
+      }
+
+      else if (objMode[key] > max) {
+        max = objMode[key];
+        ans = key;
+      }
+    }
+    console.log(ans)
+    return max;
+  }
+
+
+  if (array.length === 1) {
+    return true;
+  }
+  else {
+    return mode(array) === mean(array);
+  }
 }
 
 module.exports = modemean;
