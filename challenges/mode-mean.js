a9dbd168137c22579d5b40dd6b1b4b0b5b316fcf
Mike Sherman <himikesherman@gmail.com>
2016-11-19 10:00:34 -0800

    modemean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
-
+  var average = 0;
+  var frequency = {};
+  var maxmode = array[0];
+  array.forEach(function(num) {
+    if (!frequency[num]) {
+      frequency[num] = 0;
+    }
+    frequency[num]++;
+    average += num;
+  });
+      
+  average = Math.floor(average/array.length);  
+  for (var num in frequency) {
+    if (frequency[num] >= frequency[maxmode] && num > maxmode) {
+      maxmode = parseInt(num, 10);
+    }
+  }
+  
+  if (average === maxmode) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = modemean;
