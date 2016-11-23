99837e35e1a2743c92de928b694d6581bb46d042
Joseph Kim <canyouspelljoe@gmail.com>
2016-11-23 09:40:14 -0800

    power recursive -account for negative powers

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,21 @@
  */
 
 function pow(base, power) {
-
+  // if power is negative
+  if (power < 0) {
+    // base case
+    if (power === 0) return 1;
+    // recursive call to reach base case
+    // main difference of a negative base is to invert the number;
+    return (1 / base) * (pow(base, power + 1));
+  } 
+  // if power is positive
+  else {
+    // base case
+    if (power === 0) return 1;
+    // recursive call to reach base case
+    return base * pow(base, power - 1);
+  }
 }
 
 module.exports = pow;
