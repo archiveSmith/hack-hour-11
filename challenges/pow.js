f3ce43df0741b1f6875caa2bdfb9f4b5d977fee1
Jimmy Huynh <jkchuynh829@gmail.com>
2016-11-23 09:57:24 -0800

    removed unnecessary edge cases for pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,10 +4,10 @@
 
 function pow(base, power) {
   if (power === 0) return 1;
-  if (power === 1) return base;
-  return base * pow(base, --power);
+  if (power < 0) return 
+  return base * pow(base, power - 1);
 }
 
-console.log(pow(100, -2));
+console.log(pow(100, 1));
 
 module.exports = pow;
