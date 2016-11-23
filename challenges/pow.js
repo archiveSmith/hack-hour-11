bb0a285e7d2e48d9492e53635e8f7302b4592d9b
Mike Bieronski <mike.bieronski@gmail.com>
2016-11-23 09:39:37 -0800

    hack hour wednesday (power)

--- challenges/pow.js
+++ challenges/pow.js
@@ -1,8 +1,13 @@
 /* Write a function that calculates x^y, where x is given as the base and y is given as the power.
  * Use recursion!
  */
-
 function pow(base, power) {
+let result = 0;
+if(power === 0){
+	return 1;
+}
+
+return base * pow(base, power-1);
 
 }
 
