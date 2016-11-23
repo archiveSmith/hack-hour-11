93a04eb5079fcee0db6863ddbce5b12f335d03a8
Mike Sherman <himikesherman@gmail.com>
2016-11-23 09:37:35 -0800

    Pow completed

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,17 @@
  */
 
 function pow(base, power) {
-
+	var isNegative = 1;
+	if (power < 0) {
+		power *= -1;
+		isNegative = -1;
+	}
+	if (!power) {
+		return 1;
+	}
+	while (power) {
+		return isNegative * base * pow(base, power - 1);
+	}
 }
 
 module.exports = pow;
