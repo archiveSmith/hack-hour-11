306037b9eb5607d240d13dde3cedc9f0bcbc6a3e
David Marx <codesmith@Davids-MacBook-Air.local>
2016-11-25 21:31:40 -0800

    fix for negative powers

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,11 +3,28 @@
  */
 
 function pow(base, power) {
-  if (power < 0) {
-    return 1 / (base * pow(base, power + 1));
-  };
+  if (power === -1) return 1 / base;
   if (power === 0) return 1;
-  return base * pow(base, power - 1);
+  if (power < 0) {
+    return (1 / base) * pow(base, power + 1);
+  }
+  if (power > 0) {
+    return base * pow(base, power - 1);
+  }
 }
-
 module.exports = pow;
+
+
+/*
+for example pow(3,-3)
+
+--
+return 1/3 * 1/pow(3,-2) 1/3
+
+return 1/3 *1/pow(3,-1) 1
+
+return 1/3 * 1/pow(3,0)  == 1/3
+
+return 1
+
+*/
