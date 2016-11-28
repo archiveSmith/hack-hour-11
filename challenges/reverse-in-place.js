4f8db31d79afffbb90d0bd8c14d289a46e1beda3
Jon Kim <kimhjona@gmail.com>
2016-11-28 09:32:41 -0800

    finished reverse

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,7 +13,11 @@
  */
 
 function reverseInPlace(array) {
-
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    result.push(array[i]);
+  }
+  return result;
 }
 
 module.exports = reverseInPlace;
