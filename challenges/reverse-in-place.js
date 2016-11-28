828e57815117e577946c88e851532bf3307d6a45
Michael Richards <mrichards@Michaels-MacBook-Pro.local>
2016-11-28 09:49:02 -0800

    Added edge case

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,6 +13,9 @@
  */
 
 function reverseInPlace(array) {
+  if (!array){
+    return [];
+  }
   for (let i = 0; i < Math.floor(array.length/2); i++){
     let temp = array[i];
     array[i] = array[array.length -1 -i];
