52eec82bf412f4e885077aa1e818e8032276806e
Michael Richards <mrichards@Michaels-MBP.lan>
2016-11-28 09:40:15 -0800

    Hack Hour - Reverse in Place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,7 +13,12 @@
  */
 
 function reverseInPlace(array) {
-
+  for (let i = 0; i < Math.floor(array.length/2); i++){
+    let temp = array[i];
+    array[i] = array[array.length -1 -i];
+    array[array.length -1 -i] = temp;
+  } 
+  return array;      
 }
 
 module.exports = reverseInPlace;
