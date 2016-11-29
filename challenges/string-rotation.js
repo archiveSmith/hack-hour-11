057d76932ac2b5002ccbe8389ca21961377c076d
Vincent King <vincentking@Vincents-MacBook-Air.local>
2016-11-29 10:00:50 -0800

    hackhour string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,37 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+	//iterate over every element in array 
+	//check if following element is in proper sequence
+	//if not return false
+	//if i is greater than word.length-1 go back to 0;
+
+
+	//this is challenging because we don't know where to start...
+
+	//how about this?
+	//we can find the first letter, keep checking the next to see if it's in order
+	//if not, find the other letter that matches the first letter, if no other foubd, return false 
+
+	let newS1 = s1.split("");
+	let newS2 = s2.split("");
+
+	for (var i = 0;i < newS2.length-1; i++){
+		if (newS2[i] === newS1[0]){
+			for (var j = i; j-1;j++){
+				if (j > newS2.length-1){
+					j = 0
+				}
+				if (newS2[j] !== newS1[])
+
+			}
+		}
+	}
+	//doesnt work, need to go back and fix it!!
+
+	
 
 }
 
+//console.log(stringRotation("hello","llohe"))
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
