f7dbabdbbe27c494cb0857ec24db698b8b19554a
nickiowaspain <nickholtan@gmail.com>
2016-11-28 23:02:06 -0800

    finished reverse

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -11,9 +11,17 @@
  * Since strings are immutable in javascript, we will be reversing an array of characters instead.
  *
  */
-
+// don't use methods
 function reverseInPlace(array) {
-
+  let temp = [];
+  for(let i =array.length; i >= 0; i--) {
+   temp.push(array[i]);
+  }
+  for(let i = 0; i < temp.length; i++){
+    array[i] = temp[i];
+  }
+  
+return array;
 }
 
 module.exports = reverseInPlace;
