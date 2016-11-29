46b54049373e70b296e66cb300acad47d0ad2c7f
listentomm <maxmarcususc@gmail.com>
2016-11-29 09:59:04 -0800

    strin rotation brute force solutiong

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Given two strings, write a function to check if the second one is a rotation of the first,
  * using only one call to isSubstring. The function isSubstring takes in two strings and returns
  * true if the second string is found in the first, false otherwise.
@@ -16,7 +17,22 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+	if (typeof s1 !== 'string' || typeof s2 !== 'string') return undefined;
+	if (s1.length < 1 || s2.length < 1) return false;
+	if (s1 === s2) return true;
+	let s2arr = s2.split('');
+	let targetIndex = s2arr.indexOf(s1[0]);
+	let count = 0;
 
+	while (count < targetIndex - 1) {
+		let num = s2arr.pop();
+		s2arr.unshift(num);
+		count += 1;
+	}
+
+	const s2edit = s2arr.join('')
+
+	return s1 === s2edit;
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
