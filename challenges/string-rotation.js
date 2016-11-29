fb1738fc410e8817101a5d0c6c5ff3e2658c7412
dhanimay <mayfield_dhani@dhanimay.com>
2016-11-29 09:33:04 -0800

    add string-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,9 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+	if(!s1 || !s2){ return false}
+	if(s1.length !== s2.length) return false;
+	return isSubstring(s1, s2.split("").reverse().join(""));
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
