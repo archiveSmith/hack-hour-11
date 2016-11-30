81f814bc28aabd3079a6de617e18256f0e0590d2
Christopher Cano <canoc4262@gmail.com>
2016-11-30 09:59:35 -0800

    Finish initial solution to balanced parens hack hour challenge.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,43 @@
  *
  */
 
-function balancedParens(input){
+ function matchParens(openParens, closedParens) {
+  if (openParens === '(' && closedParens === ')') {
+    return true;
+  } else if (openParens === '{' && closedParens === '}') {
+    return true;
+  } else if (openParens === '[' && closedParens === ']') {
+    return true;
+  }
 
+  return false;
 }
+
+function balancedParens(input) {
+
+  const strippedInput = input.replace(/[^\[\]\(\){}]/g, '');
+  const len = strippedInput.length;
+
+  if (len <= 1) {
+    return true;
+  }
+
+  const stack = [];
+
+  for (let i = 0; i < len; i += 1) {
+    if (/[\[\({]/.test(strippedInput[i])) {
+      stack.push(strippedInput[i]);
+    } else {
+      const popped = stack.pop();
+
+      if (!matchParens(popped, strippedInput[i])) {
+        return false;
+      }
+    }
+  }
+
+  return !stack.length;
+}
+
 
 module.exports = balancedParens;
