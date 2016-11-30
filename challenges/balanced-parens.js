1a2944eb00ae9ac89b82f432bc8211e46cc6fff8
dhanimay <mayfield_dhani@dhanimay.com>
2016-11-30 09:56:25 -0800

    hack hour

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
  * write a function that takes a string of text and returns true if
  * the parentheses are balanced and false otherwise.
@@ -24,8 +25,24 @@
  *
  */
 
+let brackets = { "[": "]", "{": "}", "(": ")"};
+let values = { "]": "[", "}": "{", ")": "("};
+
 function balancedParens(input){
+	return input.split("").reduce((open, char, index, array) =>{
+		if(char in brackets){ open.unshift(brackets[char]) }
+
+		if(char == open[0]){
+		  open.shift(); 
+		}else if(char in values){
+		  open.unshift(char);
+		}
+
+		return open;
+
+	}, []).length === 0;
+
 
 }
-
+//console.log(balancedParens('[({)})]'));
 module.exports = balancedParens;
--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -18,7 +18,7 @@ function isSubstring(s1, s2) {
 function stringRotation(s1, s2) {
 	if(!s1 || !s2){ return false}
 	if(s1.length !== s2.length) return false;
-	return isSubstring(s1, s2.split("").reverse().join(""));
+	return isSubstring(s1, s2 + s2);
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
