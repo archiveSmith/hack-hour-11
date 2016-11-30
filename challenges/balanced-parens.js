7eb0006bc23dfa5834c8caeb4ff91e157e2c4d87
Joseph Kim <canyouspelljoe@gmail.com>
2016-11-30 10:00:43 -0800

    step 1 of balanced parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,65 @@
  */
 
 function balancedParens(input){
+  // // Step 2
+  // let lparen = 0;
+  // let rparen = 0;
+  // let lsquare = 0;
+  // let rsquare = 0;
+  // let lcurly = 0;
+  // let rcurly = 0;
+  // for (let i = 0; i < input.length; i++) {
+  //   if (input[i] === '(') {
+  //     lparen += 1;
+  //   }
+  //   if (input[i] === ')') {
+  //     if (lparen > rparen) {
+  //       rparen += 1;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  //   if (input[i] === '[') {
+  //     lsquare += 1;
+  //   }
+  //   if (input[i] === ']') {
+  //     if (lsquare > rsquare) {
+  //       rsquare += 1;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  //   if (input[i] === '{') {
+  //     lcurly += 1;
+  //   }
+  //   if (input[i] === '}') {
+  //     if (lcurly > rcurly) {
+  //       rcurly += 1;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  // }
+  // if (lparen === rparen && lsquare === rsquare && lcurly === rcurly) return true;
+  // return false;
 
+  // Step 1
+  let lparen = 0;
+  let rparen = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(') {
+      lparen += 1;
+    }
+    if (input[i] === ')') {
+      if (lparen > rparen) {
+        rparen += 1;
+      } else {
+        return false;
+      }
+    }
+  }
+  if (lparen === rparen) return true;
+  return false;
 }
 
 module.exports = balancedParens;
