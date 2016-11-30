0bde9c0bc3f9a623b5e62cc7fe777f35326b3e32
Michael Richards <mrichards@MacBook-Pro-3.local>
2016-11-30 09:34:26 -0800

    master

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,40 @@
  */
 
 function balancedParens(input){
+  if (!input) return false;
 
+ const storage = [];
+  const pairs = {
+    '[':']',
+    '{':'}',
+    '(':')'
+  };
+  const closers = {
+    ')': 1,
+    ']': 1,
+    '}': 1
+  };
+  
+  //loop through the string
+  for(let i = 0; i < input.length; i++){
+    let cur = input[i];
+    // if we hit a paren, push it to storage
+    if(pairs[cur]){
+      storage.push(pairs[cur]);
+    } 
+    // if we hit a closing paren, we see if it's opener exists in storage
+    // if it does, we pop it out of the storage
+    // if it doesn't we return false
+    else if(cur in closers){
+      if(storage[storage.length -1] === cur){
+        storage.pop();
+      } else{
+        return false;
+      }
+    }
+  }
+  // if true, storage should be empty, so this boolean should return true;
+  return !storage.length;
 }
 
 module.exports = balancedParens;
