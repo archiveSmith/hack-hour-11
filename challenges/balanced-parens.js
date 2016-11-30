6ff15b4a2f9310e85f1a03c7372338b7346646ad
Jimmy Huynh <jkchuynh829@gmail.com>
2016-11-30 09:47:55 -0800

    balanced parents brute force

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,39 @@
  */
 
 function balancedParens(input){
-
+  // COUNT HOW MANY OF EACH PARENTS
+  const counter = {};
+  // FILTER OUT EVERY OTHER CHAR
+  const test = '[](){}';
+  const tally = input
+    .split('')
+    .filter(x => test.split('').includes(x));
+  // EARLY CHECK FOR ODD NUMBER OF PARENTS
+  if (tally.length % 2 !== 0) return false;
+  // MATCH PAIRS AND REPLACE
+  const result = tally.map(x => {
+    switch(x) {
+      case ']':
+        return '['
+      case ')':
+        return '('
+      case '}':
+        return '{'
+      default:
+        return x;
+    }
+  });
+  // POPULATE COUNTER
+  result.forEach(x => {
+    counter[x] = counter[x] + 1 || 1;
+  });
+  // CHECK IF VALUES ARE EVEN, WHICH MEANS THEY ARE PAIRED
+  const keys = Object.keys(counter);
+  for (let i = 0; i < keys.length; i ++) {
+    if (counter[keys[i] % 2 !== 0]) return false;
+  }
+  // RETURN TRUE IF ALL PASS
+  return true;
 }
 
 module.exports = balancedParens;
