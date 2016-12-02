09a1a35c78bd1ee19d143191c869c527453e8a81
dhanimay <mayfield_dhani@dhanimay.com>
2016-12-02 13:27:03 -0800

    hack hour roman numeral complete

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Given a positive integer, return it as a string in Roman Numeral form.
  * Some basic conversions are given below:
  *
@@ -14,11 +15,20 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
+let rome = [{1000: "M"}, {900: "CM"}, {500: "D"}, {400: "CD"}, {100: "C"}, {90: "XC"}, {50: "L"}, {40: "XL"}, {10: "X"}, {5: "V"}, {4: "IV"}, {1:"I"}];
+function romanNumeral(n, i) {
+  if(n === 0 || i === rome.length) return "";
+  if( i === undefined) i = 0;
+  let key = Object.keys(rome[i])[0];
+  let leftover = n - key;
 
-function romanNumeral(n) {
-
+  if(leftover >= 0){
+    return rome[i][key] + romanNumeral(leftover, i);
+  }else{
+    return romanNumeral(n, i+1);
+  }
 }
 
 module.exports = romanNumeral;
