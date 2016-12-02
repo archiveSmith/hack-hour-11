f2cafe1c51919ded463e88a4cd8d82ce25899255
Joseph Kim <canyouspelljoe@gmail.com>
2016-12-02 10:01:01 -0800

    roman-numeral kinda working

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -16,9 +16,38 @@
  *      1000  ->    M
  * 
  */
-
 function romanNumeral(n) {
-
+  let numeral = '';
+  let value = n;
+  while (value > 0) {
+    console.log(value);
+    if (value >= 1000) {
+      numeral += 'M';
+      value -= 1000;
+    } else if (value >= 500) {
+      numeral += 'D';
+      value -= 500;
+    } else if (value >= 100) {
+      numeral += 'C';
+      value -= 100;
+    } else if (value >= 50) {
+      numeral += 'L';
+      value -= 50;
+    } else if (value >= 10) {
+      numeral += 'X';
+      value -= 10;
+    } else if (value >= 5) {
+      numeral += 'V';
+      value -= 5;
+    } else {
+      numeral += 'I';
+      value -= 1;
+    } 
+    
+    let check = numeral.slice(numeral.length - 4)
+    // if ()
+  }
+  return numeral;
 }
 
 module.exports = romanNumeral;
