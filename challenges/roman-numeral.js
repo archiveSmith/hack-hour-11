69bc1bbedc41c94e199bedb244083d12bae777db
Mike Bieronski <mike.bieronski@gmail.com>
2016-12-03 00:17:34 -0800

    friday hack hour

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,43 @@
  */
 
 function romanNumeral(n) {
+	let answer = "";
+	while(n >= 1000){
+		answer += "M";
+		n = n-1000;
+	}
+	while(n >= 500){
+		answer += "D";
+		n = n-500;
+	}
+	while(n >= 100){
+		answer += "C";
+		n = n-100;
+	}
+	while(n >= 50){
+		answer += "L";
+		n = n-50;
+	}
+	while(n >= 10){
+		answer += "X";
+		n = n-10;
+	}while(n >= 5){
+		answer += "V";
+		n = n-5;
+	}
+	while(n >= 1){
+		answer += "I";
+		n = n-1;
+	}
+	answer = answer.replace("DCCCC", "CM");
+	answer = answer.replace("CCCC", "CD");
+	answer = answer.replace("LXXXX", "XC");
+	answer = answer.replace("XXXX", "XL");
+	answer = answer.replace("VIIII", "IX");
+	answer = answer.replace("IIII","IV");
+
+	
+	return answer;
 
 }
-
 module.exports = romanNumeral;
