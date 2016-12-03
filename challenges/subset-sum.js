558355466ee4051dde43fbc9adffa2ec4d69c2e5
Anto <antoafarian@gmail.com>
2016-12-03 10:00:30 -0800

    subset sum finished

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,36 @@
  */
 
 function subsetSum(array, target) {
+    if (!array || !target) {
+        return false;
+    }
 
+    let sortedArr = array.sort(function(a,b){ return a-b; });
+    let sum = 0;
+
+    let filteredArr = sortedArr.filter(function(value) { return value <= target; })
+
+
+    console.log(filteredArr);
+    
+    for (let i=filteredArr.length-1; i>=0; i--) {
+        sum += filteredArr[i];
+        console.log('sum is at: ' + sum);
+        if (sum === target) {
+            return true;
+        }
+        if (sum > target) {
+            sum = 0;
+            console.log('sum was greater, now its ' + sum);
+            console.log('splicing ' + filteredArr[i+1]);
+            filteredArr.splice(i+1);
+            console.log(filteredArr);
+            i++;
+        }
+    } 
+    return false;
 }
+
+//console.log(subsetSum([12,5,2,9,4,2,5,22,15,6], 16));
 
 module.exports = subsetSum;
