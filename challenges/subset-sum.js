bc57e86d0c5ce23d29697f54a56d8f5811fc7293
Mike Bieronski <mike.bieronski@gmail.com>
2016-12-03 13:32:10 -0800

    saturday hack hour

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,60 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+// function subsetSum(array, target) {
+// let total = 0;
+// while(array.length > 0){
+// 	total = array[0];
+// 	for(let i = 1; i < array.length; i++){
+// 		total = total + array[i];
+// 		if(total === target) {
+// 		 return true;
+// 		} else if( total > target){
+// 			total = 0;
+// 			break;
+// 		}
+// 	}
+// 	array.shift();
+// }
+// return false;
+// }
 
+function subsetSum(array, target) {
+	let start  = 0
+	let time = start
+	while(time < start + 5000){
+		let index = Math.floor(Math.random()*array.length);
+		let index2 = Math.floor(Math.random()*array.length);
+		if(index === index2) Math.floor(Math.random()*array.length);
+		if(array[index] + array[index2] === target) return true;
+	time++;
+	
+ 	}
+
+	while(time >= (start + 5000) && time < (start + 10000)){
+		let index = Math.floor(Math.random()*array.length);
+		let index2 = Math.floor(Math.random()*array.length);
+		let index3 = Math.floor(Math.random()*array.length);
+		if(index === index2) index2 = Math.floor(Math.random()*array.length);
+		if(array[index3] + array[index] + array[index2] === target) return true;
+	time++;
+	
+	}
+	
+		while(time >= start + 10000 && time < start + 20000){
+		let index = Math.floor(Math.random()*array.length);
+		let index2 = Math.floor(Math.random()*array.length);
+		let index3 = Math.floor(Math.random()*array.length);
+		let index4 = Math.floor(Math.random()*array.length);
+  	if(index === index2) index2 = Math.floor(Math.random()*array.length);
+		if(index2 === index4) index4 = Math.floor(Math.random()*array.length);
+		if(array[index3] + array[index] + array[index2] + array[index4] === target) return true;
+	time++;
+	
+	}
+
+	return false;
 }
+
 
 module.exports = subsetSum;
