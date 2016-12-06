7805d67cd4e806bbcdea97584793438d7df48030
Mike Sherman <himikesherman@gmail.com>
2016-12-06 09:45:06 -0800

    Syntax update

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -18,7 +18,7 @@ function binToDec(binary) {
     //start countining columns based on position of first non 0 position
     binary = binary.substr(binary.indexOf("1"));
     let dec = 0;
-    for (let col = binary.length - 1; col >= 0; col --) {
+    for (let col = binary.length - 1; col >= 0; col--) {
       if (binary[col] === "1") {
         dec += Math.pow(2, col);
       }
