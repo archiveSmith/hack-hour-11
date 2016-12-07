1b635e20ba9a5f6837e17ace138902a1d21115a9
listentomm <maxmarcususc@gmail.com>
2016-12-07 09:48:28 -0800

    match-word brute force completed

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,3 +1,4 @@
+'use strict';
 // Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
@@ -8,7 +9,20 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+	const lowCaseStr = str.toLowerCase();
+	let lowCaseArr = lowCaseStr.split(/[^a-z]/);
+	let checkArr = [];
+	lowCaseArr = lowCaseArr
+	.filter(word => word.length > 0)
+	.forEach((word) => {
+		const len = checkArr.length;
+		const revWord = word.split('').reverse().join('');
+		if (checkArr[len -1] === revWord) checkArr.pop();
+		else (checkArr.push(word));
+	});
+	
 
+	return checkArr.length === 0
 }
 
 module.exports = matchWord;
