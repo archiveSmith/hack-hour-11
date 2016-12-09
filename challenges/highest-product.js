05376343357d6686f6c93b1ebe9b11f3bb94b218
Anto <antoafarian@gmail.com>
2016-12-09 10:00:53 -0800

    added negative number functionality

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,11 +3,26 @@
  */
 
 function highestProduct(array) {
+    let result = [];
     if (!array || array.length < 3) {
         return false;
     }
     sortedArray = array.sort(function(a,b){return b-a});
-    return sortedArray[0] * sortedArray[1] * sortedArray[2];
+    result.push(sortedArray[0]);
+    result.push(sortedArray[1]);
+    result.push(sortedArray[2]);
+
+    let len = sortedArray.length;
+    if (sortedArray[len-1] < 0 && sortedArray[len-2] < 0) {
+        if (sortedArray[len-1] * sortedArray[len-2] > result[result.length-1] * result[result.length-2]) {
+            result[result.length-1] = sortedArray[len-1];
+            result[result.length-2] = sortedArray[len-2];
+        }
+    }
+    return result;
+
 }
+
+console.log(highestProduct([1,6,5,4,9,7,-3,-9,-1]))
 
 module.exports = highestProduct;
