6f717b3ee6e510b8ab55639a5f68c54c7348b4d7
Michael Richards <mrichards@MacBook-Pro-3.local>
2016-12-09 09:48:06 -0800

    master

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -8,7 +8,7 @@ function highestProduct(array) {
   // multiply the highest number and two lowest numbers (in case they are high negatives)
   // compare the two numbers and return the higher product.
   // profit.
-  if (array.length < 3) return "boo";
+  if (array.length < 3 || !array) return "boo";
   let data = array.sort(function(a,b) { return a - b; });
   const prod1 = data[0] * data[1] * data[2];
   const prod2 = data[0] * data[data.length-1] * data[data.length-2];
