a56a6640561e908e823659b7eb94a6e7ef5bce4a
Jimmy Huynh <jkchuynh829@gmail.com>
2016-12-09 09:44:47 -0800

    first highest-product attempt

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) return array.reduce((a, b) => a * b);
+  return array
+  .sort((a, b) => a < b)
+  .slice(0, 3)
+  .reduce((a, b) => a * b);
 }
 
+const arr = [10, 3, 23, 12, 3, 5];
+
+console.log(highestProduct(arr));
 
 module.exports = highestProduct;
