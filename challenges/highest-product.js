34b20f85dad2cec4639462665e61be64ff07b41f
Anto <antoafarian@gmail.com>
2016-12-09 09:46:09 -0800

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,11 @@
  */
 
 function highestProduct(array) {
-
+    if (!array || array.length < 3) {
+        return false;
+    }
+    sortedArray = array.sort(function(a,b){return b-a});
+    return sortedArray[0] * sortedArray[1] * sortedArray[2];
 }
-
 
 module.exports = highestProduct;
