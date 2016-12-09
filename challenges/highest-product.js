3566c1bc84aaf388e87ab76c5a918d69c1fb6c3b
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2016-12-09 09:45:30 -0800

    Finished hack hour 13

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,14 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) return 0;
+  array = array.sort((a,b) => a - b);
+  let newArr = array.slice(-3);
+  let result = 1;
+  for (let i = 0; i < newArr.length; i += 1) {
+    result *= newArr[i];
+  }
+  return result;
 }
-
 
 module.exports = highestProduct;
