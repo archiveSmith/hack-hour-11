3c3dd84c0979baaf55b15a71f30068d44d6db11d
listentomm <maxmarcususc@gmail.com>
2016-12-09 09:45:24 -0800

    highest product brute force

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -1,10 +1,20 @@
+'use strict';
+
 /**
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
 function highestProduct(array) {
+  if (!Array.isArray(array)) return undefined;
+  if (array.length < 3) return 0;
 
+  const bigThree = array.sort((a, b) => a - b).slice(-3);
+  return bigThree.reduce((acc, cur) => {
+    return acc *= cur;
+  });
 }
 
-
 module.exports = highestProduct;
+
+const array = [3, 2, -1];
+console.log(highestProduct(array));
