960de9e643f82b7b0e3c4b2df7de3b6ccdd83bec
Morgan Pierson <morganpierson@gmail.com>
2016-12-09 09:54:54 -0800

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,11 +4,12 @@
 
  function highestProduct(array) {
    let result;
-   if (array.length < 3) return 0;
+   if (array.length < 3) result = 0;
+
    array = array.sort(function(a, b) {
      return a - b;
    })
-  
+
   result = array[array.length - 3] * array[array.length - 2] * array[array.length - 1]
 
    return result;
