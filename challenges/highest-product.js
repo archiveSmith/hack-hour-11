1b2f03a4c91b0d7320169a26dd80445ee4ff9e60
Christopher Cano <canoc4262@gmail.com>
2016-12-09 09:47:43 -0800

    Finish initial solution to highest product hack hour.

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,25 @@
  */
 
 function highestProduct(array) {
+  let result;
+  const len = array.length;
 
+  if (!len) {
+    return undefined;
+  } else if (len > 0 && len < 3) {
+    result = 1;
+    for (let i = 0; i < array.length; i += 1) {
+      result *= array[0];
+    }
+    return result;
+  }
+  array.sort();
+
+  const productBeginning = array[0] * array[1] * array[len - 1];
+  const productEnd = array[len - 1] * array[len - 2] * array[len - 3];
+  result = productEnd > productBeginning ? productEnd : productBeginning;
+
+  return result;
 }
 
 
