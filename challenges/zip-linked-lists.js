0c3cb8595252e746e4dea894d79c4d355103a804
Christopher Cano <canoc4262@gmail.com>
2016-12-10 22:40:01 -0800

    Finish zip-linked-list hack hour initial solution.

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,38 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let head;
+  let tail;// = head;
+
+  if (l1) {
+    head = l1;
+    //tail = l2;
+    tail = head;
+    l1 = l1.next;
+  } else {
+    head = l2;
+    tail = head;
+    l2 = l2.next;
+  }
+  //let tail = head;
+
+  while (l1 !== null || l2 !== null) {//tail !== null) {
+    if (l2) {
+      tail.next = l2;
+      l2 = l2.next;
+      //tail.next = l2;
+      //l2 = l2.next;
+      tail = tail.next;
+    }
+
+    if (l1) {
+      tail.next = l1;
+      l1 = l1.next;
+      tail = tail.next;
+    }
+  }
+
+  return head;
 };
 
 module.exports = {Node: Node, zip: zip};
