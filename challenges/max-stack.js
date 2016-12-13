2929bc9bf7970d6f1e2ea7bf522a24dd053e9d92
Morgan Pierson <morganpierson@gmail.com>
2016-12-13 09:37:10 -0800

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,32 @@
 
 function Stack() {
   // body...
+  return {
+    stack: [],
+    length: 0,
+    push: function(val) {
+      if (this.stack[0] === undefined) {
+        this.stack[0] = val;
+        this.length++;
+      } else {
+        this.stack[this.length - 1] = val;
+        this.length++;
+      }
+      return this.length;
+    },
+    pop: function() {
+      let val = this.stack[this.length - 1];
+      this.stack.length = this.stack.length - 1;
+      return val;
+    },
+    getMax: function() {
+      if (this.stack[0] === undefined) return undefined;
+      this.stack = this.stack.sort(function(a, b) {
+        return a - b;
+      })
+      return this.stack[this.length - 1];
+    }
+  }
 }
 
 module.exports = Stack;
