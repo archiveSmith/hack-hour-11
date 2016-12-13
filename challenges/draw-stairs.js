2075e90989aa53b14e9cf41e1c72f4e9e291798f
Christopher Fleming <cfleming214@gmail.com>
2016-12-13 10:01:45 -0800

    drawStairs fix

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -14,32 +14,55 @@
 */
 
 function drawStairs(n) {
-	let str = "";
-for(let i = n; i > 0; i--) {
+	let stairs = '';
+	let stairLength = n;
 
-	let difference = n - i;
-str = str + strGen(i) + "/n";
-}
+		while(n > 0) {
+			let spaces = n - 1;
+			for(let i = 0; i <= stairLength; i += 1) {
+				if(spaces > 0) {
+					stairs += ' ';
+				}
 
-function whiteGen(num) {
-		let str1 = "";
-	if(num > 0) {
-		str1 = str1 + " ";
-		whiteGen(num - 1);
-	}
-	console.log(str1);
-}
-
-function strGen(num) {
-	let str1 = "";
-	if(num > 0) {
-	str1 = str1 + "*";
-	strGen(num - 1);
-} else {
-	console.log(str1;
-}
-console.log(str1);
+				else {
+					stairs += '*';
+					spaces -= 1;
+				}
+				stairs += '\n';
+				n -= 1;
+			}
+			console.log(stairs);
+		}
 }
 
 
 module.exports = drawStairs;
+
+// function drawStairs(n) {
+	//original attempt (wrong)
+	// let str = "";
+	// for(let i = n; i > 0; i--) {
+	//
+	// let difference = n - i;
+	// str = str + strGen(i) + "/n";
+	// }
+	//
+	// function whiteGen(num) {
+	// 	let str1 = "";
+	// if(num > 0) {
+	// 	str1 = str1 + " ";
+	// 	whiteGen(num - 1);
+	// }
+	// console.log(str1);
+	// }
+	//
+	// function strGen(num) {
+	// let str1 = "";
+	// if(num > 0) {
+	// str1 = str1 + "*";
+	// strGen(num - 1);
+	// } else {
+	// console.log(str1;
+	// }
+	// console.log(str1);
+// }
