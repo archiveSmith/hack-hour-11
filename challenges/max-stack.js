df623502907c45b9935971ecf4d40b85a1fe187f
Anto <antoafarian@gmail.com>
2016-12-13 09:53:27 -0800

    finished max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,37 @@
  */
 
 function Stack() {
-  // body...
+   this.maxNum;
+   this.length = 0;
+   this.storage;
+   this.push = function (num) {
+      if (!this.storage) {
+        this.storage = [num];
+        this.maxNum = num;
+        this.length = 1;
+      }
+      else {
+        this.storage[this.storage.length] = num;
+        if (num > this.maxNum) {
+          this.maxNum = num;
+        }
+        this.length++;
+      }
+      return this.length;
+    };
+   this.pop = function () {
+      if (this.storage.length > 0) {
+        let temp = this.storage[this.storage.length-1];
+        this.storage.splice(-1, 1);
+        if (temp === this.maxNum && this.storage.length > 1) {
+          this.storage.sort(function(a,b){return a-b});
+          this.maxNum = this.storage[this.storage.length-1];
+        }
+        this.length--;
+        return temp;
+      }
+   };
+   this.getMax = function () {return this.maxNum};
 }
 
 module.exports = Stack;
