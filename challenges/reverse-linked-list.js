464b9ecd5b711dfbca76e1d61d8f917962e9ded8
Mike Bieronski <mike.bieronski@gmail.com>
2016-12-14 22:13:01 -0800

    hack hour

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,8 +13,8 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-	if(head.next === null) return head;
-	if(head.value === null) return undefined;
+	if(head.next === null || head === null) return head;
+
 	let oldHead = head;
 	let curr = head;
 	let newHead = new Node(head.value);
