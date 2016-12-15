ba9d48eee804d51e2fe5a06d8763c09f375aded5
Mike Bieronski <mike.bieronski@gmail.com>
2016-12-14 22:07:21 -0800

    wednesday hack hour

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -7,14 +7,37 @@
  * Do it in place
  *
  */
-
 function Node(value) {
     this.value = value;
     this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+	let oldHead = head;
+	let curr = head;
+	let newHead = new Node(head.value);
+	let otherCurr = newHead;
+	while(head.next !== null){
+  	while(curr.next.next !== null){
+  		curr = curr.next;
+  	}
+	otherCurr.next = curr.next;
+	otherCurr.next.next = curr;
+	curr.next = null;
+	curr = oldHead;
+	otherCurr= otherCurr.next;
+	}
+	return newHead.next;
+	
 }
+
+
+
+let a = new Node(1);
+a.next = new Node(2);
+a.next.next = new Node(3);
+a.next.next.next= new Node(4);
+
+reverseLinkedList(a);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
