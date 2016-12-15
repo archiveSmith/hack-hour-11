604a07365b78209583af1be03f359678c0eb42fe
Christopher Cano <canoc4262@gmail.com>
2016-12-15 01:04:51 -0800

    Finish initial solution to reverse linked list hack hour challenge.

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,6 +15,22 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let tail = head;
+  let prev;
+  let newHead = head.next;
+
+  while (newHead.next !== null) {
+    prev = newHead;
+    newHead = newHead.next;
+    prev.next = tail;
+    tail = prev;
+  }
+
+  return prev;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
