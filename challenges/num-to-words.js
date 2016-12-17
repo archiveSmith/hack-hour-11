fa582193d97f429cf19eb6d4787ede4b9f7fd735
Brian Rudloff <brianrudloff@gmail.com>
2016-12-17 10:00:09 -0800

    num-to-words incomplete

---
+++ challenges/num-to-words.js
@@ -0,0 +1,50 @@
+// Write a function that takes a number as an argument and returns its english word representation as a string. Answers should be in upper camel case (a.k.a. Pascal case). Don't worry about spaces.
+// Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
+
+
+/**
+ * numToWords(0) -> 'Zero'
+ * numToWords(43) -> 'FortyThree'
+ * numToWords(2999) -> 'TwoThousandNineHundredNintyNine'
+ * numToWords(15) -> 'Fifteen'
+ * numToWords(2483579411) -> 'TwoBillionFourHundredEightyThreeMillionFiveHundredSeventyNineThousandFourHundredEleven'
+ * numToWords(300525151340440) -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
+ * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
+ */
+
+function numToWords(num) {
+let numStr = num.toString();
+
+    let table = { 
+        '0': 'Zero',
+        '1': 'One',
+        '2': 'Two',
+        '3': 'Three',
+        '4': 'Four',
+        '5': 'Five',
+        '6': 'Six',
+        '7': 'Seven',
+        '8': 'Eight',
+        '9': 'Nine',
+        '10': 'Ten',
+        '11': 'Eleven',
+        '12': 'Twelve',
+        '13': 'Thirteen',
+        '14': 'Fourteen',
+        '15': 'Fifteen',
+        '16': 'Sixteen',
+        '17': 'Seventeen',
+        '18': 'Eighteen',
+        '19': 'Nineteen',
+        '20': 'Twenty'
+    }
+
+    let table2 = {}
+    if (numStr.length < 3){
+        return table[numStr]
+    }
+}
+
+
+
+module.exports = numToWords;
