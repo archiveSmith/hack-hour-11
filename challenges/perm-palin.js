fe913a4abd90b63cdf7c76f6bf0a805e9ed89082
Michael Richards <mrichards@Michaels-MacBook-Pro.local>
2016-12-19 09:34:49 -0800

    perm-palin.js

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,27 @@
  */
 
 function permPalin(str) {
-	
+  if (!str) return undefined;
+	// if even length, each letter must appear an even number of times.
+  // if odd length, each letter must appear an even number of times EXCEPT one.
+  let letterCounter = {};
+  let letter; 
+  let palindrome = 0;
+
+  // loop through the string
+  // create a counter of that letter's instance, or add 1 to existing count
+  for (let i = 0; i < str.length; i++) {
+    letter = str[i];
+    letterCounter[letter] = letterCounter[letter] || 0;
+    letterCounter[letter] ++;
+  }
+
+  // loop through each counter, most should === 0, one can === 1
+  for (let count in letterCounter) {
+    palindrome += letterCounter[count] % 2;
+  }
+  // if we return 0 or 1, it can palindrome, otherwise it's false.
+  return palindrome < 2;
 }
 
 module.exports = permPalin;
