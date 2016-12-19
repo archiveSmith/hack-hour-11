4eae623624d99ae1b00a783d7428c9406936e303
listentomm <maxmarcususc@gmail.com>
2016-12-19 09:47:29 -0800

    perm palin complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
+  if (typeof str !== 'string') return undefined;
+  const letterCounts = {};
+  const strArr = str.split('');
+  const len = strArr.length;
+  for (let i = 0; i < len; i += 1) {
+    if (letterCounts[strArr[i]]) letterCounts[strArr[i]] += 1;
+    else letterCounts[strArr[i]] = 1;
+  }
+  const evenOddCount = Object.values(letterCounts);
+  let oddCount = 0;
+  evenOddCount.forEach((num) => {
+    if (num % 2 !== 0) oddCount += 1;
+  });
+
+  return oddCount <= 1;
 }
 
 module.exports = permPalin;
