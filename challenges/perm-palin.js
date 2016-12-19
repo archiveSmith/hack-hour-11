48bab27abc44b454efa55a47b95c211f5d9e7795
Christopher Cano <canoc@ChristophersAir.lan>
2016-12-19 09:39:09 -0800

    Finish initial solution to perm-palin hack hour challenge.

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,32 @@
  */
 
 function permPalin(str) {
-	
+  if (!str || str.length < 1) {
+    return undefined;
+  }
+	const lettersObj = {};
+
+  const strArr = str.split('');
+  const strArrLen = strArr.length;
+
+  for (let i = 0; i < strArrLen; i += 1) {
+    if (!lettersObj[strArr[i]]) {
+      lettersObj[strArr[i]] = 0;
+      lettersObj[strArr[i]] += 1;
+    } else {
+      lettersObj[strArr[i]] += 1;
+    }
+  }
+
+  let numOfOdd = 0;
+
+  for (let letter in lettersObj) {
+    if (lettersObj[letter] % 2 > 0) {
+      numOfOdd += 1;
+    }
+  }
+
+  return numOfOdd <= 1;
 }
 
 module.exports = permPalin;
