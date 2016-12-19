c62edb919a6ba710fdcadf58d8cf2f13c61e813a
Mike Sherman <himikesherman@gmail.com>
2016-12-19 09:51:56 -0800

    perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,27 @@
  */
 
 function permPalin(str) {
-	
+	const charCount = {};
+	let canBePalin = true;
+	str = str.toLowerCase();
+	str.split('').forEach(function(char, i){
+		if (!charCount[char]) {
+			charCount[char] = 1;
+		} else {
+			charCount[char]++;
+		}
+	});
+
+	Object.keys(charCount).forEach(function(i){
+		if (charCount[i] % 2 !== 0) {
+			console.log("here");
+			canBePalin = false;
+		}
+	});
+
+	return canBePalin;
 }
+
+console.log(permPalin('abaccb'));
 
 module.exports = permPalin;
