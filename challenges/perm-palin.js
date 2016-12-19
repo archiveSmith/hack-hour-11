c4dffc4715dbdc2ad72af3dfc4227760172d933e
Jimmy Huynh <jkchuynh829@gmail.com>
2016-12-19 09:51:46 -0800

    num-to-words and perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,31 @@
  *
  */
 
-function permPalin(str) {
-	
+const permPalin = (str) => {
+  if (str.length === 1) return true;
+  const arr = str.split('');
+	const perms = [[]];
+  const isPalin = (perm) => perm === perm.split('').reverse().join('');
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0, len = perms.length; j < len; j++) {
+      const combo = perms[j].concat(arr[i]);
+      if (isPalin(combo.join('')) && combo.length === str.length) {
+        return true;
+      }
+      perms.push(combo);
+    }
+  }
+  return false;
 }
+
+const str1 = 'racecar'
+const str2 = 'canon'
+const str3 = 'poop'
+const str4 = 'octopus'
+console.log(permPalin(str1));
+console.log(permPalin(str2));
+console.log(permPalin(str3));
+console.log(permPalin(str4));
+
 
 module.exports = permPalin;
