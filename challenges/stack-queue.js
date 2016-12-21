76947ce93993a6dfcc0830282c3598eea4d3a310
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2016-12-21 09:37:55 -0800

    hack hour stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,18 @@
 
 
 function Stack() {
-
+  this.content = [];
+  this.length = 0;
+  this.push = function(ele) {
+    this.content[this.length] = ele;
+    this.length += 1;
+  }
+  this.pop = function() {
+    const result = this.content[this.length];
+    this.content.splice(this.content[this.length - 1], 1);
+    this.length -= 1;
+    return result;
+  }
 }
 
 
