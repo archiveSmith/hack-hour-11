fcd3384b6dc5f07e075cf30efb9cfc2f2b4e4c68
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2016-12-22 09:33:38 -0800

    hack hour two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
-
+  if (arr.length < 2) return false;
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      let sum = arr[i] + arr[j];
+      if (sum === n) return true;
+    }
+  }
+  return false;
 }
 
 module.exports = twoSum;
