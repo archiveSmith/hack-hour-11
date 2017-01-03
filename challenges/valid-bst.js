345edde17c80732a21dbdcf41eb6ff1683292506
David Marx <david.b.marx@gmail.com>
2017-01-03 09:44:14 -0800

    try 1 - Valid-BST

---
+++ challenges/valid-bst.js
@@ -0,0 +1,56 @@
+/* You are given a tree. Write a function to check if it is a valid binary search tree. A tree is
+ * a valid binary search tree if it satisfies the following constraints:
+ *      at any given node, the value of all the nodes in its left tree must be <= its value
+ *      at any given node, the value of all the nodes in its right tree must be > its value
+ */
+ 
+
+function BinaryTree(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+}
+
+BinaryTree.prototype.add = function(value) {
+  let newTree = new BinaryTree(value);
+  if (newTree.value < this.value) {
+    if (!this.left) {
+      this.left = newTree;
+    } else {
+      this.left.add(value);
+    }
+  }
+  if (newTree.value > this.value) {
+    if (!this.right) {
+      this.right = newTree;
+    } else {
+      this.right.add(value);
+    }
+  }
+};
+
+
+let newTree = new BinaryTree(4);
+newTree.add(4);
+newTree.add(100);
+newTree.add(6);
+newTree.add(12);
+newTree.add(8);
+newTree.add(-10);
+newTree.add(10);
+newTree.add(-1);
+
+
+function validBST(tree) {
+  function rec(tree){
+  if (tree.left && tree.left.value > tree.value) return false;
+  if (tree.right && tree.right.value < tree.value) return false;
+  if (tree.left && tree.left.value <= tree.value) rec(tree.left);
+  if (tree.right && tree.right.value > tree.value) rec(tree.right);
+  }
+  if (rec(tree) === undefined) return true;
+  return rec(tree);
+}
+
+
+module.exports = {BinaryTree: BinaryTree, validBST: validBST};
