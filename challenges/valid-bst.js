4f1fd365935aaed02cce484ffde54fd30c3b1756
Mike Sherman <himikesherman@gmail.com>
2017-01-03 09:54:21 -0800

    valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,7 +12,33 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+	console.log("Looking at value: ", tree.value, tree.left, tree.right);
+	if (tree.value && (tree.left || tree.right)) {
+		if (tree.left && tree.left.value <= tree.value) {
+			console.log(tree.left.value, tree.value);
+			validBST(tree.left);
+		}
 
+		if (tree.right && tree.right.value > tree.value) {
+			console.log(tree.right.value, tree.value);
+			validBST(tree.right);
+		}
+
+		return false;
+	} else {
+		return true;
+	}
 }
+
+// var bt = new BinaryTree(5);
+// bt.left = new BinaryTree(4);
+// bt.left.left = new BinaryTree(3);
+// bt.right = new BinaryTree(6);
+// bt.right.right = new BinaryTree(7);
+// bt.right.right.right = new BinaryTree(4);
+
+// console.log(bt);
+
+// console.log(validBST(bt));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
