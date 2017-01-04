9707a81d4a797acaa22d93782d056bec3014ca9a
Christopher Cano <canoc@Christophers-MacBook-Air.local>
2017-01-04 09:42:16 -0800

    Finish initial solution to gcd hack hour challenge.

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,6 +9,16 @@
 
 function gcd(a, b) {
 
+  const larger = a > b ? a : b;
+  let divisor = 1;
+
+  for (let i = 2 ; i <= larger; i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      divisor = i;
+    }
+  }
+
+  return divisor;
 }
 
 module.exports = gcd;
