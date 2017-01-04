a772b2ef35bdc285e28efc3a3842672e92a3b389
nickiowaspain <nickholtan@gmail.com>
2017-01-04 00:00:03 -0800

    turning in valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -11,8 +11,69 @@ function BinaryTree(val) {
     this.right = null;
 }
 
-function validBST(tree) {
+// function validBST(tree) {
+//   if (this.left === null && this.right === null) {
+//     return true;
+//   } else if (this.left > this.left.value || this.right.value < this.value) {
+//     return false;
+//   }
+//   if (validBST(this.left) && validBST(this.right)) {
+//     return true;
+//   }
+// }
 
+//---
+function validBST(tree) {
+   if (tree === null) return true;
+
+  if (tree.left != undefined && tree.left.value > tree.value) {
+    return false;
+  }
+
+  if (tree.right != undefined && tree.right.value <= tree.value) {
+    return false;
+  }
+
+  return validBST(tree.left) && validBST(tree.right);
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+
+// function isBST(tree) {
+//   if (tree === null) return true;
+
+//   if (tree.left != undefined && tree.left.value > tree.value) {
+//     return false;
+//   }
+
+//   if (tree.right != undefined && tree.right.value <= tree.value) {
+//     return false;
+//   }
+
+//   return isBST(tree.left) && isBST(tree.right);
+// }
+
+
+
+
+
+
+
+
+
+
+isBinary(root)
+    {
+        if root == null 
+          return true
+       else if( root.left == NULL and root.right == NULL)
+          return true
+       else if(root.left == NULL)
+           if(root.right.element > root.element)
+               rerturn isBInary(root.right)
+        else if (root.left.element < root.element)
+              return isBinary(root.left)
+        else
+              return isBInary(root.left) and isBinary(root.right)
+
+    }
