46a6a7fea4bce6ecb729432122ac77abe0619ef2
listentomm <maxmarcususc@gmail.com>
2017-01-04 09:34:16 -0800

    gcd complete

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -6,9 +6,22 @@
  * gcd(10, 9) -> 1
  *
  */
+// console.log(gcd(10, -5));
 
 function gcd(a, b) {
-
+  if (typeof a !== 'number' || typeof b !== 'number');
+  let max, min;
+  if (a > b) {
+    max = a;
+    min = b;
+  } else {
+    min = a;
+    max = b;
+  }
+  while (a % min !== 0 || b % min !== 0) {
+    min -= 1;
+  }
+  return min;
 }
 
 module.exports = gcd;
