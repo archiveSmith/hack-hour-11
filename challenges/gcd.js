5424bf4b9495cc3789c2d7b774669498bccbac94
nmarentes <nikolmarentes@gmail.com>
2017-01-04 09:48:56 -0800

    completed gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,28 @@
  *
  */
 
+f
 function gcd(a, b) {
-
+	let aDiv=[];
+	let bDiv=[];
+	let ans = 1;
+	for(let i=0;i<=a;i++){
+		if(a%i===0){
+			aDiv.push(i)
+		}
+	}
+	for(let i=0;i<=b;i++){
+		if(b%i===0){
+			bDiv.push(i)
+		}	
+	}
+	for(let i=0; i<aDiv.length; i++){
+		if(bDiv.includes(aDiv[i])){
+			ans=aDiv[i];
+		}
+	}
+	return ans
 }
+
 
 module.exports = gcd;
