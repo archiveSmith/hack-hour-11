be8d9864edcfb3a948d5ab25bc4b8ccf928c5f21
Jimmy Huynh <jkchuynh829@gmail.com>
2017-01-04 09:33:19 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
-
+  if(b === 0) return a;
+  const dividend = Math.max(a, b);
+  const divisor = Math.min(a, b);
+  const quotient = Math.floor(dividend / divisor);
+  const remainder = dividend % divisor;
+  console.log(`Quotient-Remainder Form: ${dividend} = ${divisor} x ${quotient} + ${remainder}`);
+  return gcd(divisor, remainder);
 }
+
+console.log(gcd(210, 45));
 
 module.exports = gcd;
