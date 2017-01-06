78fd6d243d6a84b78143c3d8b7e006fef3152220
Joseph Kim <canyouspelljoe@gmail.com>
2017-01-06 09:26:27 -0800

    add twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,4 +1,5 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 2222 counts as having 4 2s.
+// Write a function "countTwos" that takes a number as an argument, n. The function will return the 
+// number of 2s encountered when counting from 1 to n (inclusive). 2222 counts as having 4 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
@@ -8,7 +9,14 @@
 
 
 function countTwos(num) {
-
+  let count = 0; 
+  for (let i = 0; i <= num; i += 1) {
+    const numStr = i.toString();
+    for (let j = 0; j < numStr.length; j+= 1) {
+      if (numStr[j] === '2') count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = countTwos;
