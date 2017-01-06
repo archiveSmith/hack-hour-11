0693b546e44f77080988681ac6a24ae16b6b1e7c
Christopher Fleming <cfleming214@gmail.com>
2017-01-06 09:18:51 -0800

    apply-it

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,9 +26,16 @@
  */
 
 function applyIt(func, args) {
+  return func(
+    args[0],
+    args[1],
+    args[2],
+    args[3],
+    args[4],
+    args[5],
+    args[6]
+  );
 
-  return func.call(args);
-  
 }
 
 module.exports = applyIt;
