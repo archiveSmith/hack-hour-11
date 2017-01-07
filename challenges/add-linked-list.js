f40950d64e5523f6daf70b1c4d2c4dcd0c04091d
Joseph Kim <canyouspelljoe@gmail.com>
2017-01-07 09:56:53 -0800

    add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,55 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-
+  let sumNode = new Node(l1.value + l2.value);
+  let currNode = sumNode;
+  console.log('initial sumNode:', sumNode);
+  let curr1 = l1;
+  let curr2 = l2;
+  let carrier = 0;
+  while (curr1.next || curr2.next) {
+    if (!curr1.next) {
+      currNode.next = new Node(curr2.next.value + carrier);
+    } else if (!curr2.next) {
+      currNode.next = new Node(curr1.next.value + carrier);
+    } else {
+      if (curr1.next.value + curr2.next.value > 9) {
+        const digit = curr1.next.value + curr2.next.value - 10;
+        currNode.next = new Node(digit + carrier);
+        carrier = 1;
+      } else {
+        currNode.next = new Node(curr1.next.value + curr2.next.value + carrier);
+        carrier = 0;
+      }
+      console.log('sumNode:', sumNode);
+    }
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+    currNode = currNode.next;
+  }
+  return sumNode;
 }
+
+let ll1 = {
+  value : 3,
+  next : {
+    value : 1,
+    next: {
+      value: 5,
+      next: null
+    }
+  }
+};
+
+let ll2 = {
+  value : 5,
+  next : {
+    value : 9,
+    next: {
+      value: 2,
+      next: null
+    }
+  }
+};
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
