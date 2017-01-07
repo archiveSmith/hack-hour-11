6f87ed6fe751210ebd7b4fbe2524a4bc5ce3af7c
dhanimay <mayfield_dhani@dhanimay.com>
2017-01-07 09:36:11 -0800

    add-linked-list.js

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -1,3 +1,4 @@
+'use strict';
 /* You have two numbers represented by linked lists. Each node contains a single digit. The digits
  * are stored in reverse order, such that the 1's digit is at the head of the list. Write
  * a function that adds the two numbers and returns the sum as a linked list with the same
@@ -14,6 +15,21 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let sl1 = '', sl2 = '';
+
+  while(l1 || l2){
+    if(l1){
+      sl1 += l1.value.toString();
+      l1 = l1.next;
+    }
+
+    if(l2){
+      sl2 += l2.value.toString();
+      l2 = l2.next;
+    }
+  }
+
+  return parseInt(sl1.split('').reverse().join('')) + parseInt(sl2.split('').reverse().join(''));
 
 }
 
