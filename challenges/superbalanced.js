f4b249d94b035ea1316fa2bcd6a97d57365ff39f
Anto <antoafarian@gmail.com>
2017-01-09 10:34:17 -0800

    added edge case even though it wasn't in the tests

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -16,12 +16,19 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  if (!tree.left && !tree.right) {
+    return true;
+  }
   let deepestSoFarLeft = 0;
   let deepestSoFarRight = 0;
   let count = 1;
  
-  recurse(tree.right, count, 'right');
-  recurse(tree.left, count, 'left');
+  if (tree.right) {
+    recurse(tree.right, count, 'right');
+  }
+  if (tree.left) {
+    recurse(tree.left, count, 'left');
+  }
 
   console.log('left depth: ', deepestSoFarLeft);
   console.log('right depth: ', deepestSoFarRight);
@@ -66,7 +73,7 @@ function superbalanced(tree) {
 // tree.right.left = new BinaryTree(7);
 // tree.right.right = new BinaryTree(12);
 // tree.right.right.right = new BinaryTree(14);
-// tree.right.right.right.right = new BinaryTree(15);
+// // tree.right.right.right.right = new BinaryTree(15);
 
 // console.log(superbalanced(tree));
 
