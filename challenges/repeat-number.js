fe91c58a11ff673dfe926a6416aa699f80882822
Mike Sherman <himikesherman@gmail.com>
2017-01-10 09:18:53 -0800

    repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,18 @@
  */
 
 function repeatNumbers(array) {
-
+	var countArr = {};
+	for (var i = 0; i < array.length; i++) {
+		if (!countArr[array[i]]) {
+			countArr[array[i]] = 1;
+		} else {
+			return array[i];
+		}
+	}
 }
+
+// var arr = [1,2,3,4,7,6,7,8,9,10];
+
+// console.log(repeatNumbers(arr));
 
 module.exports = repeatNumbers;
