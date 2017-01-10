b029d23934fa52d3166e581669e16b40b318f85e
Christopher Cano <canoc@Christophers-MacBook-Air.local>
2017-01-10 01:20:14 -0800

    Finish initial solution to superbalanced hack hour challenge.

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -15,6 +15,59 @@ function BinaryTree(value) {
 
 function superbalanced(tree) {
 
+  let balanced = true;
+
+  function checkBalanced(tree) {
+
+    if (!tree.left && !tree.right) {
+      return;
+    }
+
+    if (!tree.left) {
+      if (tree.right) {
+        if (tree.right.right) {
+          //return false;
+          balanced = false;
+        }
+      }
+    } else {
+      checkBalanced(tree.left);
+    }
+
+    if (!tree.right) {
+      if (tree.left) {
+        if (tree.left.left) {
+          //return false;
+          balanced = false;
+        }
+      }
+    } else {
+      checkBalanced(tree.right);
+    }
+  }
+
+  checkBalanced(tree);
+  
+  return balanced;//checkBalanced(tree) === undefined && balanced;
 }
+
+// let tree1 = new BinaryTree(5);
+// let tree2 = new BinaryTree(6);
+// let tree3 = new BinaryTree(7);
+// let tree4 = new BinaryTree(2);
+// let tree5 = new BinaryTree(3);
+
+// let tree6 = new BinaryTree(8);
+// let tree7 = new BinaryTree(9);
+
+// tree1.left = tree4;
+// tree4.right = tree5;
+
+// tree1.right = tree2;
+// tree2.right = tree3;
+// tree3.right = tree6;
+// tree6.right = tree7;
+
+console.log(superbalanced(tree1));
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
