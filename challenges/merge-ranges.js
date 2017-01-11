3f7747255f9e4dedbdc39540ae9c064dc60d58e3
Mike Sherman <himikesherman@gmail.com>
2017-01-11 09:43:29 -0800

    merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,33 @@
 
 
 function mergeRanges(array) {
+	var merged = [];
+	var mergedPos = 0;
 
+	array = array.sort(function(a, b){
+		if (a[0] > b[0]) {
+			return 1;
+		} else {
+			return -1;
+		}
+	});
+
+	merged.push(array[0]);
+
+	for (var i = 1; i < array.length; i++) {
+		if (merged[mergedPos][1] >= array[i][0]) {
+			merged[mergedPos][1] = array[i][1];
+		} else {
+			merged.push(array[i]);
+			mergedPos++;
+		}
+	}
+
+	return merged;
 }
+
+// var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+
+// console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
