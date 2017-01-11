6b7f198fd73362ef77d516459917ebdd1b6afc24
Christopher Cano <canoc@Christophers-MacBook-Air.local>
2017-01-11 00:36:51 -0800

    Finish initial solution to repeat-number hack hour challenge.

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -12,6 +12,23 @@
 
 function repeatNumbers(array) {
 
+  // let oldSum = 0;
+  // let sum = oldSum;
+
+  // for (let i = 1; i < array.length; i += 1) {
+  //   if (Math.abs(sum - array[i]) === oldSum) {
+  //     return false;
+  //   }
+  //   sum = array[i];
+  // }
+
+  for (let i = 0; i < array.length - 1; i += 1) {
+    if (array[i] === array[i + 1]) {
+      return array[i];
+    }
+  }
+  
+  return undefined;
 }
 
 module.exports = repeatNumbers;
