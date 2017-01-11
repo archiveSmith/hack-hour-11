65dbb8b6042a0cbdccfeb3673a4f73e06a61abb7
David Marx <david.b.marx@gmail.com>
2017-01-11 10:00:35 -0800

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -8,17 +8,18 @@
  * Do not assume the ranges are in order
  */
 
- var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
 
 function mergeRanges(array) {
-  const sorted = array.sort((a, b) => a[0] - b[0])
-  let result=[];
-  for (let i =0; i < sorted.length; i += 1){
-    let range= [];
-    if (sorted[i+1] && sorted[i][1] >= (sorted[i+1][0])) {
-        range.push(sorted[i][0], sorted[i + 1][1]);
-        result.push(range);
-        i+=1;
+  const sorted = array.sort((a, b) => a[0] - b[0]);
+  const result =[];
+  for (let i = 0; i < sorted.length; i += 1) {
+    const range = [];
+    if (sorted[i + 1] && sorted[i][1] >= (sorted[i + 1][0])) {
+      let bigger = sorted[i + 1][1] > sorted[i][1] ? sorted[i + 1][1] : sorted[i][1];
+      range.push(sorted[i][0], bigger);
+      result.push(range);
+      i += 1;
     } else {
       result.push(sorted[i]);
     }
