d3db5fe9a341ad90717e51c2886441a4b9e4449f
Anto <antoafarian@gmail.com>
2017-01-11 12:13:09 -0800

    finished merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,29 @@
 
 
 function mergeRanges(array) {
+    let result = [];
 
+    let sortedArr = array.sort(function(a,b){
+        return a[0]-b[0];
+    })
+
+    result[0] = sortedArr[0];
+
+    for (let i=1; i<sortedArr.length; i++) {
+        if (sortedArr[i][0] <= result[result.length-1][1]) {
+            if (sortedArr[i][1] > result[result.length-1][1]) {
+                result[result.length-1][1] = sortedArr[i][1];
+            }
+        } else {
+            result.push(sortedArr[i]);
+        }
+
+    }
+    
+    console.log(result);
 }
+
+var times = [[0, 1], [3, 5], [4,8], [7,9], [10, 15]]
+mergeRanges(times);
 
 module.exports = mergeRanges;
