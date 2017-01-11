1439ffcf9d6d84b62219f08312a21bf0295fdae7
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-11 09:48:51 -0800

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,19 @@
 
 
 function mergeRanges(array) {
-
+  array.sort((a,b) => a[0] - b[0]);
+  let i = 0;
+  while (i < array.length -1) {
+    if (array[i][1] >= array[i + 1][0]) {
+      if (array[i][1] < array[i + 1][1]) {
+        array[i][1] = array[i + 1][1];
+      }
+      array.splice(i + 1, 1);
+    } else {
+      i++
+    }
+  }
+  return array;
 }
 
 module.exports = mergeRanges;
