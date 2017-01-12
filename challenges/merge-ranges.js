aa19ab636881d60d5d13793adb4ea98fd77e7e9f
admin <admin@admins-iMac.local>
2017-01-11 23:17:36 -0800

    finished merge ranges

---
+++ challenges/merge-ranges.js
@@ -0,0 +1,39 @@
+/**
+ * Write a function mergeRanges that takes an array of meeting time 
+ * ranges and returns an array of condensed ranges.
+ *
+ * Example:
+ * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+ *
+ * mergeRanges(times); -> [[0, 1], [3, 8], [9, 12]]
+ *
+ * Do not assume the ranges are in order
+ */
+
+
+function mergeRanges(array) {
+  if (!Array.isArray(array)) return [];
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let changesHaveBeenMade = false;
+    for (let j = 0; j < result.length; j++) {
+      if (array[i][0] <= result[j][1] && array[i][1] >= result[j][1]) {
+        if (result[j][1] < array[i][1]) {
+          result[j][1] = array[i][1];
+          changesHaveBeenMade = true;
+        }
+      }
+      if (array[i][1] >= result[j][0] && array[i][0] <= result[j][1]) {
+        if (result[j][0] > array[i][0]) {
+          result[j][0] = array[i][0];
+          changesHaveBeenMade = true;
+        }
+      }
+    }
+    if (!changesHaveBeenMade) result.push(array[i]);
+    console.log('result', result);
+    console.log('array[i]', array[i]);
+  }
+  return result;
+}
+module.exports = mergeRanges;
