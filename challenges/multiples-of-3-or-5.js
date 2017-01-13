c4ed8b241373aa65d80a5f6a6a2b18312ddfebcd
nickiowaspain <nickholtan@gmail.com>
2017-01-13 13:53:40 -0800

    hack2

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -20,7 +20,7 @@ function sumMultiplesXOrYBelowZ(x, y, z) {
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 1000; i += 1) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
