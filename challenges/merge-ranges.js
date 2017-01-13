632f571b2d5b85c5a7df55682541c69c58e53e10
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-13 09:51:40 -0800

    hack hour multiple-of-3-or-5

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,19 +11,28 @@
 
 
 function mergeRanges(array) {
-  array = array.sort((a,b) => a[0] - b[0]);
-  let arr = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i][1] >= array[i + 1][0]) {
-      let result = array[i].concat(array[i + 1]);
-      result.splice(1, result.length - 2);
-      arr.push(result);
-      i += 1;
-    } else {
-      arr.push(array[i]);
-    } 
-  }
-  return arr;
+  // array = array.sort((a,b) => a[0] - b[0]);
+  // let arr = [];
+  // for (let i = 0; i < array.length; i += 1) {
+  //   if (array[i][1] >= array[i + 1][0]) {
+  //     let result = array[i].concat(array[i + 1]);
+  //     result.splice(1, result.length - 2);
+  //     arr.push(result);
+  //     i += 1;
+  //   } else {
+  //     arr.push(array[i]);
+  //   } 
+  // }
+  // return arr;
+
+  const sortedTimes = array.sort((a,b) => a[0] - b[0]);
+  const firstTimeSlot = sortedTimes.shift();
+  return sortedTimes.reduce((merged, currentTimeSlot) => {
+    const previousEndTime = merged[merged.length - 1][1]
+    if (previousEndTime > currentTimeSlot[1]) return merged;
+    currentTimeSlot[0] > previousEndTime ? merged.push(currentTimeSlot) : merged[merged.length - 1][1] = currentTimeSlot[1];
+    return merged;
+  }, [firstTimeSlot]);
 }
 
 module.exports = mergeRanges;
