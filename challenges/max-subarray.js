36c04c87e8cfb06580126766521e13104bbe4884
Mike Sherman <himikesherman@gmail.com>
2017-01-14 09:29:49 -0800

    max subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,31 @@
  *
  */
 
-function maxSubarray(arr) {
+ function sumArray(arr) {
+ 	return arr.reduce(function(a, c) {
+ 		return a + c;
+ 	});
+ }
 
+function maxSubarray(arr) {
+	var maxSubArr;
+	var maxSum = -Infinity;
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = i + 1; j < arr.length; j++) {
+			var checkArr = arr.slice(i, j);
+			var checkSum = sumArray(checkArr);
+			if (checkSum > maxSum) {
+				maxSum = checkSum;
+				maxSubArr = checkArr;
+			}
+		}
+	}
+
+	return maxSubArr;
 }
+
+// var arr = [1, -2, 3, 10, -4, 7, 2, -5];
+
+// console.log(maxSubarray(arr));
 
 module.exports = maxSubarray;
