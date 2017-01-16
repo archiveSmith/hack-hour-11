c65e045982c03fce0773c3d2ead6af0182904a46
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-16 09:24:24 -0800

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,20 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+function anagrams(start, string) {
+  const results = [];
 
+  if ( string.length === 1) {
+    return [ start + string ];
+  } else {
+    for (let i = 0; i < string.length; i++) {
+      let result = anagrams(string[i], string.substr(0, i) + string.substr(i + 1));
+      for (let j = 0; j < result.length; j++) {
+        results.push(start + result[j]);
+      }
+    }
+    return results;
+  }
 }
 
 module.exports = anagrams;
