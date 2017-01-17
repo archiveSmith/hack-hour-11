2efdd826dd132711b61a75698954c73c6f89ae9d
dhanimay <mayfield_dhani@dhanimay.com>
2017-01-17 09:33:24 -0800

    delete-dups

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,31 @@
   */
 
 function anagrams(string) {
+  let store = [];
+  // console.log(string);
 
+
+  function recurse(index, str){
+    str = str + string[index];
+    console.log(str);
+    if(str.length === string.length){
+      store.push(str);
+      return;
+    }
+
+    let i = index, n = 0;
+    while(n < string.length){
+      recurse(++index, str);
+      i = ++i === string.length ? 0 : i;
+      n++;
+    }
+  }
+
+  recurse(0, '');
+  return store;
 }
 
+
+var result = anagrams('abc');
+console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
 module.exports = anagrams;
--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,43 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+ function Node(val){
+   this.value = val;
+   this.next = null;
+ }
 
+ function deleteDups(head) {
+   let control = head;
+   let curr;
+   let prev;
 
-function deleteDups(head) {
+   while(control !== null){
+     curr = control.next;
+     prev = control;
+     while (curr !== null) {
+       if(control.value === curr.value){
+         prev.next = curr.next;
+       }
+       curr = curr.next;
+       prev = prev.next;
+     }
+     control = control.next;
+   }
 
-}
+   return head;
+ }
+
+// let a = new Node(1);
+// let b = new Node(2);
+// let c = new Node(3);
+// let d = new Node(3);
+// let e = new Node(5);
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+//
+// console.log(deleteDups(a));
 
 module.exports = deleteDups;
