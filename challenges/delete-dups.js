1b505ccb6aa2350878aac84223efa378a59c6864
listentomm <maxmarcususc@gmail.com>
2017-01-17 09:25:36 -0800

    deletedups brute force

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,29 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
 
 function deleteDups(head) {
+  if (!head) return undefined;
+  const vals = {};
+  let prev;
+  let cur = head;
 
+  while (cur) {
+    if (vals[cur.value]) {
+      prev.next = cur.next;
+      cur = cur.next;
+    } else {
+      vals[cur.value] = 1;
+      prev = cur;
+      cur = cur.next;
+    }
+  }
+
+  return head;
 }
 
 module.exports = deleteDups;
