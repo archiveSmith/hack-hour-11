be623ed6bffa81328dc7438daed7b0f750dd6092
Scott Schwartz <scottmschwrtz@gmail.com>
2017-01-19 09:41:57 -0800

    find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,7 +10,17 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 
+
 function findInOrderedSet(arr, target) {
+  
+  while((arr.length) > 0) {
+    if (target == arr[0]) return true;
+    else if (target < arr[(arr.length - 1)/2 + (arr.length - 1)%2]) arr = arr.slice(0, ((arr.length - 1)/2 + (arr.length - 1)%2));
+    else arr = arr.slice(((arr.length - 1)/2 + (arr.length - 1)%2))
+
+  }
+  
+  return false
 
 }
 
