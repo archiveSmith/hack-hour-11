67acd1aad42e0b9df2917ac008f0c7b676527f5c
Christopher Fleming <cfleming214@gmail.com>
2017-01-20 09:50:08 -0800

    insertion-sort speed test

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,20 +3,34 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
+// //Original Solution
+// function insertionSort(array) {
+//   let length = array.length;
+//   let temp;
+//   let j;
+//
+//   for(let i = 0; i < array.length; i += 1) {
+//     temp = array[i];
+//     for(j = i - 1; j >= 0 && array[j] > temp; j -= 1) {
+//       array[j + 1] = array[j];
+//     }
+//     array[j + 1] = temp;
+//   }
+//   return array;
+// }
 
+//Codesmith solution
 function insertionSort(array) {
-  let length = array.length;
-  let temp;
-  let j;
-
-  for(let i = 0; i < array.length; i += 1) {
-    temp = array[i];
-    for(j = i - 1; j >= 0 && array[j] > temp; j -= 1) {
-      array[j + 1] = array[j];
+  for (var i = 1; i < array.length; i++) {
+    var j = i;
+    while (array[j] < array[j-1] && j > 0) {
+      var temp = array[j];
+      array[j] = array[j-1];
+      array[--j] = temp;
     }
-    array[j + 1] = temp;
   }
   return array;
 }
+
 
 module.exports = insertionSort;
