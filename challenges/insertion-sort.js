104f5d15be8b946895cd107a658aeee1f1ff4708
listentomm <maxmarcususc@gmail.com>
2017-01-20 10:03:13 -0800

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,23 @@
 
 
 function insertionSort(array) {
+  const len = array.length;
+  let temp;
 
+  for (let i = 1; i < len; i += 1) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      temp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = temp;
+      j -= 1;
+    }
+  }
+
+  return array;
 }
 
 module.exports = insertionSort;
+
+// const arr = [7, 3, 5, 1];
+// console.log(insertionSort(arr));
