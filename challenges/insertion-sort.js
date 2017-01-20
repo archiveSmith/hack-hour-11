50a2b5b7f72bc6eb198a92247bb9d668f6a16a29
Scott Schwartz <scottmschwrtz@gmail.com>
2017-01-20 09:37:14 -0800

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,23 @@
 
 
 function insertionSort(array) {
-
+  if(!array || array === []) {
+    return [];
+  }
+  
+  for (let i = 1; i < array.length; i += 1) {
+    let temp = array[i]
+    let j = i - 1;
+    
+    while(j >= 0 && array[j] > temp) {
+      array[j+1] = array[j];
+      j = j - 1;
+    }
+    
+    array[j + 1] = temp;
+  }
+  
+  return array
 }
 
 module.exports = insertionSort;
