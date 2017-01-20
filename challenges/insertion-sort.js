0ae747498866f13745f747ffac184c024a09b884
Morgan Pierson <morganpierson@gmail.com>
2017-01-20 09:45:29 -0800

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,19 @@
 
 
 
-function insertionSort(array) {
-
+function insertionSort(array, j = 0) {
+  if(j === array.length) return array;
+  let first;
+  let next;
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > array[i + 1]) {
+      first = array[i];
+      next = array[i + 1];
+      array[i] = next;
+      array[i + 1] = first;
+    }
+  }
+  return insertionSort(array, ++j);
 }
 
 module.exports = insertionSort;
