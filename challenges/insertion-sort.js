a1c1e62c16cdbcef4b4cf9d629cec3771698da99
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-20 09:22:02 -0800

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  function insertNumber(array, index, value){
+    for (var j = index; j >= 0 && array[j] > value; j--) {
+        array[j + 1] = array[j];
+    }
+    array[j + 1] = value;
+  }
+  for(var i = 0; i < array.length-1; i++){
+        insertNumber(array, i, array[i+1]);
+    }
+    return array;
 }
 
 module.exports = insertionSort;
