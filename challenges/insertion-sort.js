491c01d95e473970dec35d7b1b7d5ebe2a455952
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-20 09:42:23 -0800

    hack hour insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -1,11 +1,21 @@
-// Write a function that sorts and array using the insertion sort algorithm.
+// Write a function that sorts an array using the insertion sort algorithm.
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    let j = i;
+    while (j > 0 && array[j - 1] > array[j]) {
+      let temp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = temp;
+      j -= 1;
+      console.log(array);
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
