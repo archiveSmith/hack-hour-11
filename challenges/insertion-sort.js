5e4d86c4e7c93e38cc0152bff22281d32f174d4d
Christopher Cano <canoc@Christophers-MacBook-Air.local>
2017-01-20 09:29:33 -0800

    Finish initial solution to insertion-sort hack hour challenge.

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,33 @@
 
 
 function insertionSort(array) {
+  
+  if (!array || !array.length) {
+    return undefined;
+  }
+  
+  let i = 0;
+  let j;
+  const len = array.length;
 
+  for (i; i < len; i += 1) {
+    
+    for (j = i; j - 1 >= 0; j -= 1) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        break;
+      }
+    }
+  }
+
+  return array;
 }
+
+// const arr = [5, 10, 3, 7, 4, 21, 12, 1, 2];
+
+// console.log(insertionSort(arr));
 
 module.exports = insertionSort;
