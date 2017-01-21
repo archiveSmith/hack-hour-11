568df116488dc880041549b0e0ad8946da2e22a3
listentomm <maxmarcususc@gmail.com>
2017-01-21 10:24:02 -0800

    uniq num

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,26 @@
  *
  */
 function uniqueNumber(array) {
+  if (!Array.isArray(array) || !array.length) return undefined;
 
+// REDUCE
+  let buffer = array.reduce((acc, cur) => {
+    if (!acc[cur]) acc[cur] = 1;
+    else delete acc[cur];
+    return acc;
+  }, {})
+//// BRUTE FORCE
+  // const buffer = {};
+  // array.forEach(num => {
+  //   if (!buffer[num]) buffer[num] = 1;
+  //   else buffer[num] += 1;
+  // })
+  for (let prop in buffer) {
+    return prop;
+  }
 }
 
 module.exports = uniqueNumber;
+
+console.log(uniqueNumber([1,2,1,3,3]));
+
