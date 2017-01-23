559120fd94909b4452e48b7c60e1da89e9c4e8e5
Michael Bieronski <mike@Michaels-MacBook-Pro.local>
2017-01-23 09:39:27 -0800

    hack monday recursive

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,23 +9,41 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
-	if(array.length <= 1) return undefined;
-	let count = 0;
-	let sum = 1;
-	let work = array.slice();
-	let answer = [];
-	while(count < array.length){
-		work.splice(count,1);
-		for(let i = 0; i < work.length; i++){
-			sum = sum * work[i];
-		}
-		answer.push(sum)
-		count++;
-		work = array.slice();
-		sum = 1;
-	}	
-	return answer;
+// function getAllProducts(array) {
+// 	if(array.length <= 1) return undefined;
+// 	let count = 0;
+// 	let sum = 1;
+// 	let work = array.slice();
+// 	let answer = [];
+// 	while(count < array.length){
+// 		work.splice(count,1);
+// 		for(let i = 0; i < work.length; i++){
+// 			sum = sum * work[i];
+// 		}
+// 		answer.push(sum)
+// 		count++;
+// 		work = array.slice();
+// 		sum = 1;
+// 	}	
+// 	return answer;
+// }
+
+function getAllProducts(array, answer = [], count = 0) {
+  if(!array || array.length < 2) return 0;
+	if(count === array.length)  return answer;
+	let a = array.slice();
+	a.splice(count,1)
+	count++;
+	answer.push(a.reduce((acc, ele) => acc = acc*ele, 1));
+	return getAllProducts(array, answer, count);
+	
 }
+
+
+
+
+
+
+
 
 module.exports = getAllProducts;
