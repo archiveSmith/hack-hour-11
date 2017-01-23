8e39df527c10fc6f58460674db6d113f19c5dce6
admin <admin@admins-iMac.local>
2017-01-23 09:14:40 -0800

    products

---
+++ challenges/insertion-sort.js
@@ -0,0 +1,14 @@
+// Write a function that sorts and array using the insertion sort algorithm.
+
+// https://en.wikipedia.org/wiki/Insertion_sort
+
+
+
+function insertionSort(array) {
+  if (!Array.isArray || array.length < 2) return [];
+  for (let i = 1; i < array.length; i++) {
+    
+  } 
+}
+
+module.exports = insertionSort;
