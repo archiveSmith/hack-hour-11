35df10c0c0b79fc51cffe0b66c4e71f554ce8956
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-24 09:32:41 -0800

    linked list and edge case of yesterdays

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,30 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let node = new Node(val)
+  if (!this.head) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let current = this.head;
+  while (current) {
+    if (current.val === val) {
+      current.prev.next = current.next;
+      current.next.prev = current.prev;
+    }
+    current = current.next;
+  }
 };
+
 
 module.exports = LinkedList;
