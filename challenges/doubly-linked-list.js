2e8d0845c8afc522ce3bd9fbf6d840ec0b90cccc
David Marx <david.b.marx@gmail.com>
2017-01-24 09:40:39 -0800

    double linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,37 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function add(val) {
+  const newNode = new Node(val)
+  if (!this.head) { this.head = this.tail = newNode; return; }
+  newNode.prev = this.tail;
+  this.tail.next = newNode;
+  this.tail = newNode;
 };
+
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function remove(val) {
+  let curr = this.head;
+  while (curr) {
+    if (curr.val === val) {
+      curr.prev.next = curr.next;
+      curr.next.prev = curr.prev;
+      break;
+    }
+    curr = curr.next;
+  }
 };
+
+const dbL = new LinkedList();
+
+dbL.add(4)
+dbL.add(3)
+dbL.add(2)
+dbL.add(1)
+dbL.remove(3)
+console.log(dbL);
 
 module.exports = LinkedList;
