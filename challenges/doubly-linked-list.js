e7b72b4aacf868fb31bfee1fa2e615dfcdf5f549
nmarentes <nikolmarentes@gmail.com>
2017-01-25 10:33:15 -0800

    completed doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,14 +16,38 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
-};
+LinkedList.prototype.add = function (val) {}
+  if (this.head === null) {
+    let curr = new Node(val)
+    this.tail=curr;
+    this.head=curr;
+  } else {
+    let curr = this.tail
+   if(curr.next === null){
+    curr.next = new Node(val);
+    curr.next.prev = curr;
+     curr = curr.next;
+   }
+  }
+
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
+LinkedList.prototype.remove = function (val) {
+  let curr = this.head;
+  if (curr.val === val) {
+    curr = curr.next;
+    this.head = curr;
+    this.prev = null;
+  } else {
+    while (curr.next !== null) {
+      if (curr.next.val === val) {
+        curr.next = curr.next.next;
+        cur.next.prev = curr;
+      }
+    }
+  }
   
 };
 
