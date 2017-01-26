117892f27f777907797363951e2815d4916ce171
codesmith <student@mac-mini-7.local>
2017-01-26 09:23:51 -0800

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
+	for (var i = 0; i < array.length; i++) {
+		for (var j = 0; j < array.length; j++) {
+			if (array[j + 1] < array[j]) {
+				var temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 
+	return array;
 }
+
+// var array = [6,2,9,1,7,4,5,3,8];
+
+// console.log(bubbleSort(array));
 
 module.exports = bubbleSort;
