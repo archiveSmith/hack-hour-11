0d8217edd60eec4280e56cbf94666f3adc6777bc
Christopher Cano <canoc@Christophers-MacBook-Air.local>
2017-01-26 09:40:25 -0800

    Finish initial bubble sort hack hour challenge solution.

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -6,6 +6,32 @@
 
 function bubbleSort(array) {
 
+  if (!array.length) {
+    return [];
+  }
+
+  let i, j;
+  const len = array.length;
+
+  for (i = 0; i < len; i += 1) {
+
+    let swapped = false;
+
+    for (j = 0; j + 1 < len; j += 1) {
+      if (array[j] > array[j + 1]) {
+        swapped = true;
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+
+    if (!swapped) {
+      return array;
+    }
+  }
+  
+  return array;
 }
 
 module.exports = bubbleSort;
