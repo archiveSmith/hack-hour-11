3fd729eb339957fb81c0df876ee00b7c06db67aa
Anto <antoafarian@gmail.com>
2017-01-26 09:18:44 -0800

    finished bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,30 @@
 
 
 function bubbleSort(array) {
+    if (!array) {
+        return false;
+    }
+    
+    let temp;
+    let swaps = true;
+
+    while (swaps) {
+        swaps = false;
+        for (let i=0; i<array.length; i++) {
+            if (array[i+1]) {
+                if (array[i] > array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    swaps = true; 
+                }
+            }
+        }
+    }
+    console.log(array);
 
 }
+
+bubbleSort([1,5,3,4,6,2])
 
 module.exports = bubbleSort;
