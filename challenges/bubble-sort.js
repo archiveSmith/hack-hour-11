9acce89c04b328a7bf9ac8187c0745f71805f098
dhanimay <mayfield_dhani@dhanimay.com>
2017-01-26 09:21:30 -0800

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,24 @@
 
 
 function bubbleSort(array) {
+  let sorted = false;
+  let holder;
 
+  while(!sorted){
+    sorted = true;
+    for(let i = 0; i < array.length - 1; i++){
+      if(array[i] > array[i + 1]){
+        holder = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = holder;
+        sorted = false;
+      }
+    }
+  }
+
+  return array;
 }
 
+
+console.log(bubbleSort([4,8,3,89,12,45,76,98,12,3,4,2,5]));
 module.exports = bubbleSort;
