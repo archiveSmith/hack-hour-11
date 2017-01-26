e82765036c0506f0763f97add6fc7bca966933f2
Joseph Kim <canyouspelljoe@gmail.com>
2017-01-26 09:18:22 -0800

    bubble sort

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -22,8 +22,35 @@
  *
  */
 
+ const x1 = [4, 6, 2];
+ const y1 = [4, 6, 2];
+ const r1 = [4, 1, 1];
+ const sx1 = 6;
+ const sy1 = 6;
+ const ex1 = 2;
+ const ey1 = 2;
+
 function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
+  const circles = x.length;
+  // find how circles encapsualte start coor
+  const startCircle = [];
+  const endCircle = [];
+  for (let i = 0; i < circles; i += 1) {
+    const dist1 = Math.sqrt(Math.pow((x1[i] - sx1), 2) + Math.pow((y1[i] - sx1), 2));
+    const dist2 = Math.sqrt(Math.pow((x1[i] - ex1), 2) + Math.pow((y1[i] - ex1), 2));
+    // console.log(dist1);
+    // console.log(dist2);
+    if (dist1 <= r1[i]) startCircle.push([x1[i], y1[i], r1[i]]);
+    if (dist2 <= r1[i]) endCircle.push([x1[i], y1[i], r1[i]]);
+  }
+  for (let i = 0; i < startCircle.length; i += 1) {
+    while (endCircle.includes(startCircle[i])) {
+      
+    }
+  }
+  // console.log(startCircle);
+  // console.log(endCircle);
 
 }
-
+circleCountry(x1, y1, r1, sx1, sy1, ex1, ey1);
 module.exports = circleCountry;
