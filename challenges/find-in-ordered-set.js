c23704eda3b4f6a5a529f99ae5c54d22d38609ad
Christopher Fleming <cfleming214@gmail.com>
2017-01-27 09:08:43 -0800

    pre pull commit

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -6,8 +6,8 @@ You can do better than O(n) time
 var nums = [1, 4, 6, 7, 9, 17, 45]
 findInOrderedSet(nums, 4);  -> true
 findInOrderedSet(nums, 2);  -> false
-
  */
+
 
 
 function findInOrderedSet(arr, target) {
