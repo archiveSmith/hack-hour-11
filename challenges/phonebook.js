f1fd211a0472b498219d02dda1287fd137d3c24a
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-27 09:53:16 -0800

    hack hour phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -7,7 +7,7 @@
 *     ['alex','301-844-3421'],
 *     ['jae','301-844-1211'],
 *     ['david','301-844-0978'],
-*     ['travis','301-844-8505']
+*     ['travis','301-844-8505'],
 *     ['jasmine','1800-974-4539'],
 *   ];
 *
@@ -17,26 +17,33 @@
 * returns the persons phone number. If the person does not exists, return false.
 *
 * How efficient can you make this?
-
 * Part 2: 
 * Why are we storing names and phone numbers in an array?
 * develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
 * complete with methods to add new names and look up and remove existing entries
 */
-
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  for (let i = 0; i < jazbook.length; i += 1) {
+    if (jazbook[i][0] === name) return jazbook[i][1];
+  }
+  return false;
 }
-
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
+  let obj = {};
+  for (let i = 0; i < jazbook.length; i += 1) {
+    obj[jazbook[i][0]] = jazbook[i][1];
+  }
+  return obj;
+}
 
+makePhoneBookObject.prototype.add = function(name, phone) {
+  
 }
 
 const objectToExport = {
   findName,
   makePhoneBookObject,
 };
-
 module.exports = objectToExport;
