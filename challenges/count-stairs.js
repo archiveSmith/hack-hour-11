0aa812ca163138630412018b06f6c7091fde6046
listentomm <maxmarcususc@gmail.com>
2017-01-28 09:27:41 -0800

    count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,12 @@
  */
 
 function countStairs(n) {
-
+  if (n <= 0) return 0;
+  else if (n === 1) return 1;
+  else if (n === 2) return 2;
+  else {
+    return countStairs(n - 1) + countStairs(n - 2)
+  }
 }
 
 module.exports = countStairs;
