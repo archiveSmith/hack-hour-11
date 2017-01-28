aa1d2bd5a0a77c338f06547d1424d9b82a7ad8c1
nmarentes <nikolmarentes@gmail.com>
2017-01-28 09:35:15 -0800

    completed phonebook

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,24 @@
 
 
 function bubbleSort(array) {
-
+let n = array.length;
+//repeat
+let swapped = false;
+function swap(firstVal, secondVal){
+if(firstVal > secondVal){
+  temp = firstVal;
+  firstVal= secondVal;
+  secondVal = temp;
+  return firstVal, secondVal
+}
+}
+let reds = array.reduce((acc, curr)=>{
+  if(acc < curr){
+    return curr;
+  }else{
+    return acc;
+  }
+})
 }
 
 module.exports = bubbleSort;
