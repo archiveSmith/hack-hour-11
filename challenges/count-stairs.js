edb0ce68c1df2843abb0ff51b9fe9cac74be2fc6
Sherman Code <himikesherman@gmail.com>
2017-01-28 09:52:04 -0800

    count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,8 +14,44 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
-function countStairs(n) {
-
+function checkCombo(combo, arr) {
+	if (arr.indexOf(combo) >= 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
+function countStairs(n) {
+	var ways = [];
+	for (var i = 0; i < n; i++) {
+		var steps = 0;
+		for (var s = 1; s <= 2; s++) {
+			var combo = s;
+			for (var j = 0; j < n; j++) {
+				var added = 0;
+				console.log("combo: ", combo);
+				if (eval(combo) < n && !checkCombo(combo+"+1", ways)) {
+					combo = combo + "+1";
+					added = 1;
+				}
+
+				if (eval(combo) < n && !checkCombo(combo+"+2", ways) && !added) {
+					combo = combo + "+2";
+				}
+
+				if (eval(combo) === n) {
+					ways.push(combo);
+					break;
+				}
+			}
+		}
+			
+	}
+
+	return ways;
+}
+
+console.log(countStairs(11));
 
 module.exports = countStairs;
