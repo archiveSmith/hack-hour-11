986cbc23ca2486b56f05b3e4ce6593aff088894a
Michael Bieronski <mike@Michaels-MacBook-Pro.local>
2017-01-28 09:34:32 -0800

    saturday recursive hack

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,8 +14,33 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
-function countStairs(n) {
+function countStairs(n, opts = [], build = '') {
+	let options = opts;
+	// let numSteps = 0;
+	//let build = [];
 
+	if(n < 1){
+		if(n<0)return options;
+		let a = build
+		let isIt = false;
+		for(let i = 0; i < options.length; i++){
+			if(options[i].match(a) === -1){
+				isIt = true;
+				break;
+			}
+		}
+		if(!isIt) options.push(build);
+		return options;
+	}
+	 let c = build += '1'
+   let d =  build += '2'
+	countStairs(n-1, options, c)
+	countStairs(n-2, options, d)
+
+
+
+return options.length;
 }
+
 
 module.exports = countStairs;
