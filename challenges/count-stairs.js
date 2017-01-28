e49366cef2d32562f5a58563307f7ea23a5babb5
listentomm <maxmarcususc@gmail.com>
2017-01-28 09:34:37 -0800

    fix

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,8 +15,7 @@
  */
 
 function countStairs(n) {
-  if (n <= 0) return 0;
-  else if (n === 1) return 1;
+  if (n <= 1) return 1;
   else if (n === 2) return 2;
   else {
     return countStairs(n - 1) + countStairs(n - 2)
