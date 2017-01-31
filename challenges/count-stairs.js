5537ae98580aae08b8f199e12fe752668652a43e
listentomm <maxmarcususc@gmail.com>
2017-01-30 23:44:50 -0800

    new-intersection

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -17,9 +17,7 @@
 function countStairs(n) {
   if (n <= 1) return 1;
   else if (n === 2) return 2;
-  else {
-    return countStairs(n - 1) + countStairs(n - 2)
-  }
+  else return countStairs(n - 1) + countStairs(n - 2)
 }
 
 module.exports = countStairs;
