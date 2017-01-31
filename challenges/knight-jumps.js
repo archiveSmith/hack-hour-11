b6f5cd61f00060588a4ea37f7e7a28948a177b73
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-31 09:41:22 -0800

    working on my knight jumps

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -11,7 +11,31 @@
 // var str = "(4 5)"
 
 function knightjumps(str) {
+  const arr = str.split('');
+  const x = Number(arr[1]);
+  const y = Number(arr[3]);
+  let total = 0;
 
+  const moves = [
+    {x: -2, y: -1},
+    {x: -2, y: 1},
+    {x: -1, y: -2},
+    {x: -1, y: 2},
+    {x: 1, y: -2},
+    {x: 1, y: 2},
+    {x: 2, y: -1}
+    {x: 2, y: 1}
+  ]
+
+    for (let i = 0; i < moves.length; i++) {
+      const move_x = x + moves[i].x;
+      const move_y = y + moves[i].y;
+
+      if (move_x > 0 && move_x <= 8 && move_y > 0 && move_y <=8) {
+        total++;
+      }
+    }
+    return total;
 }
 
 module.exports = knightjumps;
