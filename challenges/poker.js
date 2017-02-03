e0aeabc7e1ecd7d6357562ef1be2a774459d8096
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-02-03 10:08:11 -0800

    hack hour poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,7 +19,57 @@
 */
 
 function poker(hand1, hand2) {
-
+  hand1 = hand1.sort((a,b) => a - b);
+  hand2 = hand2.sort((a,b) => a - b);
+  const table1 = {};
+  const table2 = {};
+  const result1 = [];
+  const result2 = [];
+  for (let i = 0; i < hand1.length; i += 1) {
+    table1[hand1[i]] ? table1[hand1[i]] += 1 : table1[hand1[i]] = 1;
+  }
+  for (let j = 0; j < hand2.length; j += 1) {
+    table2[hand2[j]] ? table2[hand2[j]] += 1 : table2[hand2[j]] = 1;
+  }
+  for (let card1 in table1) {
+    if (table1[card1] === 4) {
+      for (let k = 0; k < 4; k += 1) {
+        result1.push(card1);
+      }
+    }
+    if (table1[card1] === 3) {
+      for (let l = 0; l < 3; l += 1) {
+        result1.push(card1);
+      }
+    }
+    if (table1[card1] === 2) {
+      for (let m = 0; m < 2; m += 1) {
+        result1.push(card1);
+      }
+    }
+  }
+  for (let card2 in table2) {
+    if (table2[card2] === 4) {
+      for (let n = 0; n < 4; n += 1) {
+        result2.push(card2);
+      }
+    }
+    if (table2[card2] === 3) {
+      for (let p = 0; p < 3; p += 1) {
+        result2.push(card2);
+      }
+    }
+    if (table2[card2] === 2) {
+      for (let r = 0; r < 2; r += 1) {
+        result2.push(card2);
+      }
+    }
+  }
+  if (result1.length === result2.length) {
+    if (result1[0] === result1[3] && result2[0] === result2[3]) {
+      return result1[0] > result2[0] ? "Player 1 wins" : "Player 2 wins";
+    }
+  }
 }
 
 module.exports = poker;
