fe5d4b113eecebbf07d362391a19484ba9a6a6c1
nmarentes <nikolmarentes@gmail.com>
2017-02-03 10:13:10 -0800

    poker complete

--- challenges/poker.js
+++ challenges/poker.js
@@ -17,9 +17,42 @@
 *
 * Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
 */
-
 function poker(hand1, hand2) {
+	let one = ofAKind(hand1)
+	let two = ofAKind(hand2)
+  function ofAKind(hand) {
+  	let ans = [];
+    let obj = {};
+    for (let i = 0; i < hand.length; i++) {
+      !obj[hand[i]] ? obj[hand[i]] = 1 : obj[hand[i]] += 1;
+    }
+    let keys = Object.keys(obj)
+   for(let i=0; i<keys.length; i++){
+     if(obj[keys[i]]>=2){
+     	let str = keys[i] + ', ' + obj[keys[i]];
+       ans.push(keys[i])
+       ans.push(obj[keys[i]])
+     }
+   }
+	return ans;
+  }
 
-}
+ if(one[1]>two[1]){
+ 	console.log('hi')
+ 	 return 'Player 1 Wins'
+ }else if(one[1]<two[1]){
+ 	return 'Player 2 Wins'
+ }else if(one[1]===two[1]){
+ 	if(Number(one[0])> Number(two[0])){
+ 		return 'Player 1 Wins'
+ 	}else if (one[0]< two[0]){
+ 			return 'Player 2 Wins'
+ 	}else{
+ 		return 'Its a draw'
+ 	}
+ }
+} 
+
+// poker([3,8,8,8,2], [4,8,7,8,8])
 
 module.exports = poker;
