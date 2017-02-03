c9d2dddb004c0df112e3c499684689093a4a5702
Anto <antoafarian@gmail.com>
2017-02-03 11:07:46 -0800

    finished poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,7 +19,123 @@
 */
 
 function poker(hand1, hand2) {
+    values = {
+        'Four of a Kind': 7,
+        'Full House': 6,
+        'Straight': 5,
+        'Three of a Kind': 4,
+        'Two Pair': 3,
+        'Pair': 2,
+        'High Card': 1,
+    }
+
+    // if (values[computeBest(hand1)[0]] > values[computeBest(hand2)[0]]) return 'Player 1 wins with a ' + computeBest(hand1)[0] + '!';
+    // if (values[computeBest(hand1)[0]] < values[computeBest(hand2)[0]]) return 'Player 2 wins with a ' + computeBest(hand2)[0] + '!';
+
+    // if (values[computeBest(hand1)[0]] === values[computeBest(hand2)[0]]){
+    //     if (computeBest(hand1)[1] > computeBest(hand2)[1]) return 'Player 1 wins with a ' + computeBest(hand1)[0] + ' with high card ' + computeBest(hand1)[1] + '!';
+    //     if (computeBest(hand1)[1] < computeBest(hand2)[1]) return 'Player 2 wins with a ' + computeBest(hand2)[0] + ' with high card ' + computeBest(hand2)[1] + '!';
+    //     if (computeBest(hand1)[1] === computeBest(hand2)[1]) return 'DRAW!';
+    // };
+
+    if (values[computeBest(hand1)[0]] > values[computeBest(hand2)[0]]) return 'Player 1 wins';
+    if (values[computeBest(hand1)[0]] < values[computeBest(hand2)[0]]) return 'Player 2 wins';
+
+    if (values[computeBest(hand1)[0]] === values[computeBest(hand2)[0]]){
+        if (computeBest(hand1)[1] > computeBest(hand2)[1]) return 'Player 1 wins';
+        if (computeBest(hand1)[1] < computeBest(hand2)[1]) return 'Player 2 wins';
+        if (computeBest(hand1)[1] === computeBest(hand2)[1]) return 'DRAW!';
+    };
+
+    function computeBest(hand) {
+        let poss = {
+          fourOfAKind: 0,
+          fullHouse: 0,
+          straight: 0,
+          threeOfAKind: 0,
+          twoPair: 0,
+          pair: 0,
+          highCard: 0,
+        }
+        let currStraight = 0;
+        let result = [];
+        
+        for (let i=0; i<hand.length; i++) {
+            // console.log('card is: ', hand[i])
+            if (hand[i] > poss['highCard']) {
+                    poss['highCard'] = hand[i];
+                }
+            if (result[hand[i]] === undefined){
+                result[hand[i]] = 1;
+            } else {
+                result[hand[i]]++;
+            }
+            
+            if (result[hand[i]] === 4) {
+                // console.log('found 4 of a kind')
+                poss['fourOfAKind'] = hand[i];
+                return ['Four of a Kind', poss['fourOfAKind']];
+            }
+            if (result[hand[i]] === 3) {
+                // console.log('found 3 of a kind')
+                if (poss['pair'] > 0 && poss['pair'] !== hand[i]) {
+                    // console.log('fullhouse')
+                    poss['fullHouse'] = Math.max(poss['pair'], hand[i]);
+                }
+                poss['threeOfAKind'] = hand[i];
+            }
+            if (result[hand[i]] === 2) {
+                // console.log('found pair')
+                if (poss['threeOfAKind'] > 0) {
+                    // console.log('fullhouse')
+                    poss['fullHouse'] = Math.max(poss['threeOfAKind'], hand[i]);
+                }
+                if (poss['pair'] > 0) {
+                    // console.log('found 2 pairs')
+                    poss['twoPair'] = Math.max(poss['pair'], hand[i]);
+                }
+                
+                poss['pair'] = hand[i];
+            }
+                
+            
+        }
+
+
+        for (let i=0; i<result.length; i++) {
+            if (result[i] > 0) {
+                currStraight++;
+            }
+            if (currStraight === 5) {
+                poss['straight'] = i;
+                break;
+            }
+            if (currStraight === 4 && i === 5 && result[14] > 0) {
+                poss['straight'] = i;
+                break;
+            } else {
+                currStraight = 0;
+            }
+        }
+ 
+        // console.log(poss);
+        if (poss['fullHouse'] > 0) return ['Full House', poss['fullHouse']]; 
+        else if (poss['straight'] > 0) return ['Straight', poss['straight']]; 
+        else if (poss['threeOfAKind'] > 0) return ['Three of a Kind', poss['threeOfAKind']]; 
+        else if (poss['twoPair'] > 0) return ['Two Pair', poss['twoPair']]; 
+        else if (poss['pair'] > 0) return ['Pair', poss['pair']];
+        else if (poss['highCard'] > 0) return ['High Card', poss['highCard']];
+
+
+
+
+        console.log(result);
+    }
+
+    
 
 }
 
+
+console.log(poker([2,4,4,4,4],[2,8,2,2,8]));
 module.exports = poker;
