99b6176a1aea4058c69642ecee7cf1dd65bbf77b
Morgan Pierson <morganpierson@gmail.com>
2017-02-06 21:27:42 -0800

    not complete

--- challenges/parse-dates.js
+++ challenges/parse-dates.js
@@ -25,22 +25,50 @@
 //   you should simply return a Date object for the current time.
 
 // Example input, assuming the current Date is Thursday December 17, 2015:
-// parseDates('Jan 12th 1:09 AM')  => returns new Date object representing 'Mon Jan 12 2015 01:09:00 GMT-0800 (PST)'
-// parseDates('Today 8:15 PM')     => returns new Date object representing 'Thu Dec 17 2015 20:15:00 GMT-0800 (PST)'
-// parseDates('Sunday 12:59 PM')   => returns new Date object representing 'Sun Dec 13 2015 12:59:00 GMT-0800 (PST)'
+// parseDates('Jan 12th 1:09 AM')  => returns new Date object representing
+//'Mon Jan 12 2015 01:09:00 GMT-0800 (PST)'
+// parseDates('Today 8:15 PM')     => returns new Date object representing
+//'Thu Dec 17 2015 20:15:00 GMT-0800 (PST)'
+// parseDates('Sunday 12:59 PM')   => returns new Date object representing
+//'Sun Dec 13 2015 12:59:00 GMT-0800 (PST)'
 
 // parseDates('Jan 1st')
 // parseDates('hello')
-// parseDates('Today 2 PM')        => invalid formats, so all return Date object for today at the current time: 'Thu Dec 17 2015 11:31:00 GMT-0800 (PST)'
+// parseDates('Today 2 PM')        => invalid formats, so all return Date object
+//for today at the current time: 'Thu Dec 17 2015 11:31:00 GMT-0800 (PST)'
 
 // FAQ:
 // - seconds / milliseconds of the returned Date object do not matter
 // - you can assume the provided day of month will be valid if it's a month string
-//   (i.e. the function will not be called with 'Jul 84th 1:00 PM') since that's not a real date
+//   (i.e. the function will not be called with 'Jul 84th 1:00 PM') since that's
+//not a real date
 // - if any part of the date string is missing then you can consider it an invalid date
 
 function parseDates(str) {
-  
+  let months = {
+    'Jan': 0,
+    'Feb': 1,
+    'Mar': 2,
+    'Apr': 3,
+    'May': 4,
+    'Jun': 5,
+    'Jul': 6,
+    'Aug': 7,
+    'Sep': 8,
+    'Oct': 9,
+    'Nov': 10,
+    'Dec': 11
+  }
+
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+'Saturday', 'Today']
+
+  let i = 0;
+  let firstWord = '';
+  while(str[i] !== ''){
+    firstWord += str[i];
+    i++;
+  }
 }
 
 module.exports = parseDates;
