6ec56991e328de7cb63a8ce2f23924ee9716c3d2
Sherman Code <himikesherman@gmail.com>
2017-02-09 09:46:42 -0800

    common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,7 +13,82 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
+	var round = {};
+	var common = {};
+	var ce = [];
 
+	for (var i = 0; i < array1.length; i++) {
+		if (!round[array1[i]]) {
+			round[array1[i]] = 1;
+		}
+	}
+
+	for (var key in round) {
+		if (!common[key]) {
+			common[key] = 0;
+		}
+		common[key] += round[key];
+	}
+
+	round = {}
+
+	for (var i = 0; i < array2.length; i++) {
+		if (!round[array2[i]]) {
+			round[array2[i]] = 1;
+		}
+	}
+
+	for (var key in round) {
+		if (!common[key]) {
+			common[key] = 0;
+		}
+		common[key] += round[key];
+	}
+
+	round = {}
+
+	for (var i = 0; i < array3.length; i++) {
+		if (!round[array3[i]]) {
+			round[array3[i]] = 1;
+		}
+	}
+
+	for (var key in round) {
+		if (!common[key]) {
+			common[key] = 0;
+		}
+		common[key] += round[key];
+	}
+
+	round = {}
+
+	for (var i = 0; i < array4.length; i++) {
+		if (!round[array4[i]]) {
+			round[array4[i]] = 1;
+		}
+	}
+
+	for (var key in round) {
+		if (!common[key]) {
+			common[key] = 0;
+		}
+		common[key] += round[key];
+	}
+
+	console.log(round);
+	console.log("common: ", common);
+
+	for (var key in common) {
+		if (common[key] >= 4) {
+			ce.push(key);
+		}
+	}
+
+	return ce;
 }
+
+//Yes, I should refactor and consolidate founds into separate function!
+
+// console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
