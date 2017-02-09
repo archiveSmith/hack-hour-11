fd3aaf092459322952101cfb0818235da62b51d0
Jimmy Huynh <jkchuynh829@gmail.com>
2017-02-09 09:47:32 -0800

    common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,30 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
-
+function removeDupes(arr) {
+  return arr.filter((element, index, array) => {
+    return !array.slice(0, index).includes(element);
+  });
 }
+
+function commonElements(...args){
+  const obj = {};
+
+  args.forEach((array, index) => {
+    array.forEach(e => {
+      obj[e] = obj[e] ? obj[e].concat(index) : obj[e] = [index];
+      obj[e] = removeDupes(obj[e]);
+    });
+  });
+
+  return Object.keys(obj).filter(e => obj[e].length === 4).map(e => parseInt(e) || e);
+}
+
+const array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+const array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+const array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+const array4 = ['ferret',12,12,45,9,66,77,78,2000];
+
+console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
