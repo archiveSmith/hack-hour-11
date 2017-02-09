bb1f55b8b223aeb6d05449a3f365a7f955b1bba2
David Marx <david.b.marx@gmail.com>
2017-02-09 09:31:49 -0800

    comment elements

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,8 +9,21 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack(items, weightAvailable) {
 
+function solveKnapsack(items, weightAvailable, score = 0) {
+  // items.forEach((el)=> el['score'] = (el.value/el.weight) )
+  // items.sort((a,b) =>  b.score - a.score)
+  // for (let i = 0; weightAvailable && i < items.length; i++){
+  //   weightAvailable > i.weight ? 
+  // }
+  console.log(score, weightAvailable)
+  if (weightAvailable <= 0) return 0;
+  if (weightAvailable === 0) return score;
+  if (items.length) console.log("BB", solveKnapsack(items.slice(1), (weightAvailable - items[0].weight), score + items[0].value), solveKnapsack(items.shift(), score))
 };
 
+
+let items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+
+console.log(solveKnapsack(items, 110))
 module.exports = solveKnapsack;
