13bb9114be36e94908886df98a7da6b114223600
Michael Bieronski <mike@Michaels-MacBook-Pro.local>
2017-02-09 23:00:41 -0800

    hack

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -14,7 +14,8 @@
 
 function commonElements(array1, array2, array3, array4){
 	let answer = [];
-	array1.reduce((acc, ele, index) => {
+	let testArray = array1.length > 0 ? array1 : array2.length > 0 ? array2 : array3.length > 0 ? array3 : array4.length > 0 ? array4 : "Nothing in Common!";
+	testArray.reduce((acc, ele, index) => {
 		if(array2.indexOf(ele) !== -1 && array3.indexOf(ele) !== -1 && array4.indexOf(ele) !== -1){
 			if(answer.indexOf(ele) === -1){
 			answer.unshift(ele);
@@ -23,6 +24,7 @@ function commonElements(array1, array2, array3, array4){
 	});
   return answer.length > 0 ? answer : "Nothing in Common!";
 }
+
 
 
 module.exports = commonElements;
