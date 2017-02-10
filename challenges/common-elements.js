85421454d221d2922e7005a93440793018d6d9bc
Michael Bieronski <mike@Michaels-MacBook-Pro.local>
2017-02-10 01:28:36 -0800

    final singleline

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,7 +13,7 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-	  return (array1.length === 0 ? [] : array2.length === 0 ? [] : array3.length === 0 ? [] : array4.length === 0 ? [] : [array1, array2, array3, array4].reduce((acc, ele) => ele.length < acc.length ? ele : acc, arguments[0])).reduce((acc, ele) => array1.indexOf(ele) !== -1 ? array2.indexOf(ele) !== -1 ? array3.indexOf(ele) !== -1 ? array4.indexOf(ele) !== -1 ?  acc === "Nothing in Common!" ? acc = [ele] : acc.indexOf(ele) !== -1 ? acc : acc.concat([ele]) : acc: acc: acc: acc, "Nothing in Common!");
+	  return [...arguments].reduce((acc, ele) => acc.length === 0 ? acc = [] : ele.length < acc.length ? ele : acc, arguments[0]).reduce((acc, ele) => array1.indexOf(ele) !== -1 ? array2.indexOf(ele) !== -1 ? array3.indexOf(ele) !== -1 ? array4.indexOf(ele) !== -1 ?  acc === "Nothing in Common!" ? acc = [ele] : acc.indexOf(ele) !== -1 ? acc : acc.concat([ele]) : acc: acc: acc: acc, "Nothing in Common!");
 }
 
 
