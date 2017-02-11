539eb0557fa8524b3265577fe2f8818ae41726dc
Anto <antoafarian@gmail.com>
2017-02-10 19:58:10 -0800

    finished each permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -12,6 +12,7 @@ No return value is necessary.
 eachPermutation([1, 2, 3], function(perm) {
   console.log(perm)
 });
+
 [ 1, 2, 3 ]
 [ 1, 3, 2 ]
 [ 2, 1, 3 ]
@@ -21,9 +22,38 @@ eachPermutation([1, 2, 3], function(perm) {
 */
 
 function eachPermutation(arr, callback) {
+  if (!arr || !callback) {
+    return false;
+  }
+  if (arr.length === 1) {
+    return callback(arr[0]);
+  }
 
+  let total = [];
+  
+  permute(arr, []);
+
+  for (let i=0; i<total.length; i++) {
+    callback(total[i]);
+  }
+ 
+  function permute(arr, newSlice, first) {
+
+    if (arr.length === 0) {
+      total.push(newSlice)
+    }
+    for (let i=0; i<arr.length; i++) {
+      // console.log('i is ' + i)
+      let com = (arr.slice(0,i).concat(arr.slice(i+1)));
+      permute(com, newSlice.concat([arr[i]]), arr[i]);
+    }
+    return total;
+  }
 }
 
+// eachPermutation([1,2,3,4], function(item){
+//   console.log(item, 'i did it')
+// });
 
 
 module.exports = eachPermutation;
