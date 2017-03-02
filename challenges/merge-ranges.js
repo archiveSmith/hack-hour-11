f2768214d2eb5a040e6f3af38d1aaf803a3ff8e7
Jimmy Huynh <jkchuynh829@gmail.com>
2017-03-02 13:45:47 -0800

    merge-range include condition for encapsulated times

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -14,12 +14,12 @@ function mergeRanges(arr) {
   .sort((a, b) => a[0] - b[0])
   .reduce((acc, cur, i, a) => {
     if (!acc[acc.length - 1]) acc.push(cur);
-    if (cur[0] >= acc[acc.length - 1][1] && cur[1] <= acc[acc.length - 1][0]) return acc;
+    if (cur[0] > acc[acc.length - 1][0] && cur[1] < acc[acc.length - 1][1]) return acc;
     if (cur[0] > acc[acc.length - 1][1]) acc.push(cur);
     if (cur[0] <= acc[acc.length - 1][1]) acc[acc.length - 1][1] = acc[acc.length - 1][1][1] > cur[1] ? acc[acc.length - 1][1][1] : cur[1];
     return acc;
   }, []);
 }
 
-console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]));
+// console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]));
 module.exports = mergeRanges;
