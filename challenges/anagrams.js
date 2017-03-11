cb0da57c254c0caef203bf5443f777039871ef7a
listentomm <maxmarcususc@gmail.com>
2017-03-10 19:18:36 -0800

    anagrams edge case

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,8 +13,8 @@
   */
 
 function anagrams(string, cur = '', cache = {}) {
-  if (typeof string !== 'string') return [''];
-  if (!string.length) return cache[cur] = true;
+  if (typeof string !== 'string') return undefined;
+  if (!string.length) cache[cur] = true;
   for (let i = 0; i < string.length; i += 1) {
     const selected = string.charAt(i);
     const rest = string.slice(0, i) + string.slice(i + 1);
