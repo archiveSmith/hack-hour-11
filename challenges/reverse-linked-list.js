95773e842f07ca4c647fbb3d78979912c4148583
Christopher Cano <canoc4262@gmail.com>
2017-03-27 16:57:38 -0700

    Re do reverse linked list.

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,25 +13,73 @@ function Node(value) {
     this.next = null;
 }
 
+// function reverseLinkedList(head) {
+
+//   if (!head || !head.next) {
+//     return head;
+//   }
+
+//   let tail = head;
+//   let prev;
+//   let newHead = head.next;
+
+//   while (newHead.next !== null) {
+//     prev = newHead;
+//     newHead = newHead.next;
+//     prev.next = tail;
+//     tail = prev;
+//   }
+//   newHead.next = tail;
+
+//   return newHead;
+// }
+
 function reverseLinkedList(head) {
+
+  // if (!head || !head.next) {
+  //   return head;
+  // }
+
+  // const curr = head;
+
+  // let temp1;
+  // let temp2;
+
+  // while (curr && curr.next) {
+
+  //   temp1 = curr.next.next;
+  //   temp2 = curr.next;
+
+  //   //if (curr.next.next) {
+  //     curr.next.next = curr;
+  //   //}
+
+  //   if (curr === head) {
+  //     curr.next = null;
+  //   }
+
+  //   curr = temp2;
+  // }
 
   if (!head || !head.next) {
     return head;
   }
-
-  let tail = head;
-  let prev;
-  let newHead = head.next;
-
-  while (newHead.next !== null) {
-    prev = newHead;
-    newHead = newHead.next;
-    prev.next = tail;
-    tail = prev;
+  
+  let reversed;
+  let prev = null;
+  let curr = head;
+  
+  while (curr) {
+    reversed = curr;
+    //console.log('rev ' + rev.val);
+    curr = curr.next;
+    //console.log('curr ' + curr.val);
+    reversed.next = prev;
+    prev = reversed;
   }
-  newHead.next = tail;
-
-  return newHead;
+  
+  return reversed;
 }
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
